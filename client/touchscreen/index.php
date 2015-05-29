c1062aa9786444a1f610f326fb0db7cf47c5cc78
stufisher <stu@q3f.org>
2015-05-29 14:26:32 +0100

    move index.html -> php for touchscreen

---
+++ client/touchscreen/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html>
+    
+    <head>
+        <link rel="stylesheet" href="jquery.mobile-1.4.5.css">
+        <link rel="stylesheet" href="mobile.css">
+
+        <script type="text/javascript" data-main="js/main" src="js/vendor/require/require.js"></script>
+
+        <title></title>
+                
+    </head>
+    
+    <body>
+
+    </body>
+</html>
