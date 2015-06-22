e5a9d039afe729ce6d0a957964c4be6eb3169d9b
stufisher <stu@q3f.org>
2015-06-22 10:37:29 +0100

    make dat plot pollable

--- client/js/modules/types/gen/dc/models/dat.js
+++ client/js/modules/types/gen/dc/models/dat.js
@@ -2,10 +2,35 @@ define(['backbone'], function(Backbone){
     
     return Backbone.Model.extend({
         urlRoot: '/dc/dat',
-        
-        parse: function(r, options) {
-            return { data: r }
-        },
+
+        initialize: function(options) {
+			this.timestamp = options.timestamp
+			this.bind('sync', this.poll, this)
+			this.refresh_thread = null
+			this.running = true
+		},
+			                                   
+		parse: function(r, options) {
+			return { data: r }
+		},
+
+		stop: function() {
+			clearTimeout(this.refresh_thread)
+			this.running = false
+		},
+			                                   
+		poll: function() {
+			clearTimeout(this.refresh_thread)
+			if (this.running) {
+				var refresh = true
+				var d = this.get('data')
+				if (d && d[0].length > 0) {
+				  if ((new Date() - this.timestamp) > (900*1000)) refresh = false
+				}
+
+				if (refresh) this.refresh_thread = setTimeout(this.fetch.bind(this), 10000)
+			}
+		},
     })
        
 })
