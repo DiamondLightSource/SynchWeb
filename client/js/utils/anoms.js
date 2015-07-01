c5417507f816c6fab20191a34ccbe53b2ecaf0a4
stufisher <stu@q3f.org>
2015-07-01 15:49:41 +0100

    update anom list

--- client/js/utils/anoms.js
+++ client/js/utils/anoms.js
@@ -2,11 +2,11 @@ define([], function() {
     
     return {
         opts: function() {
-            return _.map(this.list, function(s) { return '<option value="'+s+'">'+s+'</option>' }).join()
+            return _.map(this.list.sort(), function(s) { return '<option value="'+s+'">'+s+'</option>' }).join()
         },
         obj: function() {
             var ob = {}
-            _.each(this.list, function(sg) {
+            _.each(this.list.sort(), function(sg) {
                 ob[sg] = sg
             })
 
@@ -14,10 +14,39 @@ define([], function() {
         },
 
         list: ['',
-        "Mn", 
-        "Se", 
-        "S",
-        "Zn"
+        "Cr",
+        "Mn",
+        "Fe",
+        "Co",
+        "Ni",
+        "Cu",
+        "Zn",
+        "Ga",
+        "Ge",
+        "As",
+        "Se",
+        "Br",
+        "Kr",
+        "Rb",
+        "Mo",
+        "Ru",
+        "Pd",
+        "Ag",
+        "Cd",
+        "In",
+        "Te",
+        "I",
+        "Xe",
+        "Ta",
+        "W",
+        "Os",
+        "Pt",
+        "Au",
+        "Hg",
+        "Pb",
+        "Sm",
+        "Gd",
+        "Ho",
         ]
         
     }
