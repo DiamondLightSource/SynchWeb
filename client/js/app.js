a86372b8c501f8eba08adb5b4c21ee083773aa7a
Stu Fisher <stu@q3f.org>
2015-09-03 16:25:51 +0200

    Save current proposal type to global

--- client/js/app.js
+++ client/js/app.js
@@ -150,7 +150,7 @@ function(Backbone, Marionette, _, $, HeaderView, SideBarView, DialogRegion, Prop
       success: function(resp) {
           app.user = resp.user
           app.staff = resp.is_staff
-          app.type = resp.type
+          app.type = resp.ty
 
           // Breadcrumbs collection
           if (!app.bc) {  
