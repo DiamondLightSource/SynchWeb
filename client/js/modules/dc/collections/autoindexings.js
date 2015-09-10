e3650a210b11adbf0e3345e63731495cea915f25
Stu Fisher <stu@q3f.org>
2015-09-10 10:04:57 +0100

    Somehow missed XOAlign strategies when porting...

--- client/js/modules/dc/collections/autoindexings.js
+++ client/js/modules/dc/collections/autoindexings.js
@@ -15,6 +15,14 @@ define(['underscore', 'backbone', 'modules/dc/models/autoindexing'], function(_,
       _.each(r[1], function(e, t) {
         if (e.STRATS.length) data.push({ TYPE: t, CELL: e.CELL, STRATS: e.STRATS })
       })
+
+
+      var xo = { TYPE: 'XOAlign', STRATS: [] }
+      _.each(r[2], function(a, i) {
+        xo.STRATS.push(a)
+      })
+      if (xo.STRATS.length) data.unshift(xo)
+
       return data
                                     
     },
--- client/js/modules/dc/views/autoindexing.js
+++ client/js/modules/dc/views/autoindexing.js
@@ -1,6 +1,18 @@
-define(['marionette', 'modules/dc/collections/autoindexings', 'tpl!templates/dc/dc_strategy.html'], function(Marionette, AutoIndexings, template) {
+define(['marionette', 
+  'modules/dc/collections/autoindexings', 
+  'tpl!templates/dc/dc_strategy.html',
+  'tpl!templates/dc/dc_xoalign.html',
+  ], function(Marionette, AutoIndexings, template, xotemplate) {
        
-  var AutoIndexingItem = Marionette.ItemView.extend({ template: template, modelEvents: { 'change': 'render' } })
+  var AutoIndexingItem = Marionette.ItemView.extend({ 
+      // template: template, 
+      modelEvents: { 'change': 'render' },
+
+      getTemplate: function(m) {
+        console.log('st type', this.model.get('TYPE'), this.model.get('TYPE') == 'XOAlign')
+          return this.model.get('TYPE') == 'XOAlign' ? xotemplate : template
+      }
+  })
        
   var EmptyStrategies = Marionette.ItemView.extend({ template: '<div>No strategies available for this data collection</div>' })
        
