54fda173044afdd1138c9e0fcea57508addf2d3b
Stu Fisher <stu@q3f.org>
2016-01-11 10:12:33 +0100

    changes to mysql abstraction

--- api/includes/class.mysql.php
+++ api/includes/class.mysql.php
@@ -22,42 +22,126 @@
 
 
         function paginate($query, $args=array()) {
+            // MySQL is Limit Start Row, Number
+            // Oracle subselect is Start Row, End Row
+            if (sizeof($args)) $args[sizeof($args)-1] = $args[sizeof($args)-1] - $args[sizeof($args)-2];
             return $this->pq("$query LIMIT ?,?", $args);
         }
         
 
-        function oracle2mysql($query) {
+        function oracle2mysql($query, $args) {
+            // Allow for Oracle style :1, :2 out of order
+            preg_match_all('/\:(\d+)/', $query, $mat);
+            $rearranged_args = array();
+            if ($this->debug) print_r(array('old', $args));
+            foreach ($mat[1] as $id) {
+                $aid = $id-1;
+                $val = $args[$aid];
+                array_push($rearranged_args, $val);
+                unset($args[$aid]);
+            }
+
+            foreach ($args as $remain) {
+                array_push($rearranged_args, $remain);
+            }
+
+            if ($this->debug) print_r(array('rearr', $rearranged_args));
+            $args = $rearranged_args;
+
             // Replace oracle :1 placeholder with ?
             $query = preg_replace('/\:\d+/', '?', $query);
 
             // Date to string conversion
+            $query = preg_replace('/TO_CHAR\((.+?)MM(.+?)\)/', 'TO_CHAR(\1%m\2)', $query);
+            $query = preg_replace('/TO_DATE\((.+?)MM(.+?)\)/', 'TO_DATE(\1%m\2)', $query);
+
             $query = preg_replace('/TO_CHAR/', 'DATE_FORMAT', $query);
+            $query = preg_replace('/TO_DATE/', 'STR_TO_DATE', $query);
             $query = preg_replace('/DD/', '%d', $query);
-            $query = preg_replace('/MM/', '%m', $query);
+            
             $query = preg_replace('/YYYY/', '%Y', $query);
             $query = preg_replace('/HH24/', '%H', $query);
-            $query = preg_replace('/MI/', '%i', $query);
+            $query = preg_replace('/:MI/', ':%i', $query);
+            $query = preg_replace('/:SS/', ':%s', $query);
+            $query = preg_replace('/"T"/', 'T', $query);
 
             // String aggregation
             $query = preg_replace('/string_agg\(.?\)/', "GROUP_CONCAT(\1 SEPARATOR ',')", $query);
 
             // Timestamps
-            $query = preg_replace('/SYSDATE/', 'CURDATE()', $query);
+            $query = preg_replace('/SYSDATE/i', 'CURDATE()', $query);
 
-            return $query;
+            // Replace sequences with auto increments
+            $query = preg_replace('/s_\w+\.nextval/', 'NULL', $query);
+
+            // Replace Returning, mysql provides last insert id
+            $query = preg_replace('/ RETURNING \w+ INTO :id/', '', $query);
+
+            return array($query, $args);
         }
 
 
         function tablelookup($query) {
-            $tables = array('phpsession' => 'PHPSession',
-                            'person' => 'Person',
-                            'permission' => 'Permission',
-                            'log4stat' => 'Log4Stat',
+            $tables = array('AdminActivity',
+                            'Log4Stat',
+                            'PHPSession',
+
+                            'Person',
+                            'Permission',
+                            'UserGroup_has_Permission',
+                            'UserGroup',
+                            'UserGroup_has_Person',
+
+                            'Proposal',
+                            'BLSession',
+                            'Session_has_Person',
+
+                            'DataCollection',
+                            'EnergyScan',
+                            'BLSample_has_EnergyScan',
+                            'XFEFluorescenceSpectrum',
+                            'RobotAction',
+                            'DataCollectionGroup',
+                            'DataCollectionComment',
+                            'ImageQualityIndicators',
+                            '',
+                            'AutoProcIntegration',
+                            'AutoProcScaling_has_Int',
+                            'AutoProcScaling',
+                            'AutoProc',
+                            'AutoProcScalingStatistics',
+                            'AutoProcProgram',
+                            '',
+                            'Screening',
+                            'ScreeningOutput',
+                            'ScreeningStrategy',
+                            'ScreeningStrategyWedge',
+                            '',
+                            '',
+
+                            'BLSample',
+                            'DiffractionPlan',
+                            'BLSampleImage',
+                            'Protein',
+                            'Crystal',
+                            'Container',
+                            'Dewar',
+                            'Shipping',
+                            '',
+                            'BLSubSample',
+                            '',
+                            'BF_fault',
+                            'BF_component',
+                            'BF_subcomponent',
+                            'BF_system',
+                            '',
+                            '',
+                            '',
                 );
 
-            foreach ($tables as $lower => $case) {
-                $query = str_replace(" $lower ", " $case ", $query);
-                $query = str_replace(" $lower\n", " $case\n", $query);
+            foreach ($tables as $table) {
+                $query = str_replace(" ".strtolower($table)." ", " $table ", $query);
+                $query = str_replace(" ".strtolower($table)."\n", " $table\n", $query);
             }
 
             return $query;
@@ -65,15 +149,15 @@
         
         
         function pq($query, $args=array()) {
-            $query = $this->oracle2mysql($query);
+            list($query, $args) = $this->oracle2mysql($query, $args);
             $query = $this->tablelookup($query);
 
             if ($this->debug) {
                 print '<h1 class="debug">MySQL Debug</h1>';
                 print SqlFormatter::format($query);
                 print_r($args);
-                error_log($query);
-                error_log(print_R($args));
+                // error_log($query);
+                // error_log(print_R($args));
             }
 
             $stmt = $this->conn->prepare($query);
@@ -116,6 +200,8 @@
                     $data[] = $c;
                 }
             }
+
+            if ($this->debug) print_r(array('rows', sizeof($data)));
 
             // Need mysqlnd for this :(
             // $result = $stmt->get_result();
