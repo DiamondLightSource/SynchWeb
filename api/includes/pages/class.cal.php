73c99b31b8fb6badeb06a6f05836e72f567fb395
Stu Fisher <stu@q3f.org>
2016-01-14 18:21:43 +0100

    mysql calendar export

--- api/includes/pages/class.cal.php
+++ api/includes/pages/class.cal.php
@@ -14,16 +14,14 @@
             if ($this->has_arg('prop') || $this->has_arg('bl')) {
                 $arg = $this->has_arg('bl') ? $this->arg('bl') : $this->arg('prop');
 
-                // $args = $this->db->pq("SELECT parametercomments as p FROM genericdata WHERE parametervaluestring LIKE :1", array($arg));
-                $args = $this->db->pq("SELECT hash FROM calendarhash WHERE key LIKE :1", array($arg));
+                $args = $this->db->pq("SELECT hash FROM calendarhash WHERE ckey LIKE :1", array($arg));
                 
                 if (sizeof($args)) {
                     $this->_output('/cal/ics/h/'.$args[0]['HASH'].'/calendar.ics');
                 } else {
                     $h = md5(uniqid());
-                    // $this->db->pq("INSERT INTO genericdata (genericdataid,parametervaluestring,parametercomments) VALUES (s_genericdata.nextval, :1, :2)", array($arg, $h));
-                    $this->db->pq("INSERT INTO calendarhash (calendarhashid,key,hash) 
-                        VALUES (s_calendarhash.nextval, :1, :2)", array($arg, $h));
+                    $this->db->pq("INSERT INTO calendarhash (calendarhashid,ckey,hash,beamline) 
+                        VALUES (s_calendarhash.nextval, :1, :2, :3)", array($arg, $h, $this->has_arg('bl') ? 1 : 0));
                     
                     $this->_output('/cal/ics/h/'.$h.'/calendar.ics');
                 }
@@ -33,46 +31,38 @@
         
         # Calendar ics export
         function _export_ics() {
-            global $mx_beamlines;
-
             $where = '';
             $args = array(date('Y'));
             
             if (!$this->has_arg('h')) $this->_error('No proposal specified', 'You must specify a proposal to view a calendar');
-            // $hash = $this->db->pq("SELECT parametervaluestring as p FROM genericdata WHERE parametercomments LIKE :1", array($this->arg('h')));
-            $hash = $this->db->pq("SELECT key FROM calendarhash WHERE hash LIKE :1", array($this->arg('h')));
+            $hash = $this->db->pq("SELECT ckey,beamline FROM calendarhash WHERE hash LIKE :1", array($this->arg('h')));
             
             
             if (!sizeof($hash)) $this->_error('No proposal specified', 'The specified proposal doesnt appear to exist');
-            $arg = $hash[0]['KEY'];
+            $hash = $hash[0];
             
-            if (in_array($arg, $mx_beamlines)) {
+            if ($hash['BEAMLINE']) {
                 $where .= ' AND s.beamlinename LIKE :'.(sizeof($args)+1);
-                array_push($args, $arg);
+                array_push($args, $hash['CKEY']);
                 
             } else {
-                $where = ' AND p.proposalcode||p.proposalnumber=:'.(sizeof($args)+1);
-                array_push($args, $arg);
+                $where = ' AND CONCAT(p.proposalcode,p.proposalnumber) LIKE :'.(sizeof($args)+1);
+                array_push($args, $hash['CKEY']);
             }
             
-            $visits = $this->db->pq("SELECT s.beamlineoperator as lc, p.proposalcode || p.proposalnumber || '-' || s.visit_number as vis, p.proposalcode || p.proposalnumber as prop, s.beamlinename as bl, TO_CHAR(s.startdate, 'DD-MM-YYYY') as d, TO_CHAR(s.enddate, 'DD-MM-YYYY') as e, TO_CHAR(s.startdate, 'HH24:MI') as st, TO_CHAR(s.enddate, 'HH24:MI') as en, s.sessionid FROM blsession s INNER JOIN proposal p ON (p.proposalid = s.proposalid) WHERE (s.beamlinename LIKE 'i02' OR s.beamlinename LIKE 'i03' OR s.beamlinename LIKE 'i04' OR s.beamlinename LIKE 'i04-1' OR s.beamlinename LIKE 'i24') AND s.startdate > TO_DATE(:1,'YYYY') $where ORDER BY s.startdate, s.beamlinename", $args);
+            $visits = $this->db->pq("SELECT s.beamlineoperator as lc, CONCAT(CONCAT(CONCAT(p.proposalcode, p.proposalnumber), '-'), s.visit_number) as vis, CONCAT(p.proposalcode, p.proposalnumber) as prop, s.beamlinename as bl, TO_CHAR(s.startdate, 'DD-MM-YYYY') as d, TO_CHAR(s.enddate, 'DD-MM-YYYY') as e, TO_CHAR(s.startdate, 'HH24:MI') as st, TO_CHAR(s.enddate, 'HH24:MI') as en, s.sessionid 
+                FROM blsession s 
+                INNER JOIN proposal p ON (p.proposalid = s.proposalid) 
+                WHERE s.startdate > TO_DATE(:1,'YYYY') $where ORDER BY s.startdate, s.beamlinename", $args);
             
-            // $user_tmp = $this->db->pq("SELECT u.name,u.fullname,s.sessionid 
-            //     FROM blsession s 
-            //     INNER JOIN proposal p ON (p.proposalid = s.proposalid) 
-            //     INNER JOIN investigation@DICAT_RO i ON lower(i.visit_id) = p.proposalcode||p.proposalnumber||'-'||s.visit_number 
-            //     INNER JOIN investigationuser@DICAT_RO iu on i.id = iu.investigation_id 
-            //     INNER JOIN user_@DICAT_RO u on u.id = iu.user_id 
-            //     WHERE (s.beamlinename LIKE 'i02' OR s.beamlinename LIKE 'i03' OR s.beamlinename LIKE 'i04' OR s.beamlinename LIKE 'i04-1' OR s.beamlinename LIKE 'i24') AND s.startdate > TO_DATE(:1,'YYYY') AND iu.role='NORMAL_USER' $where", $args);
-            
-            $bls = implode("', '", $mx_beamlines);
-            $user_tmp = $this->db->pq("SELECT pe.login, pe.givenname || '-' || pe.familyname as fullname
+
+            $user_tmp = $this->db->pq("SELECT s.sessionid, pe.login, CONCAT(CONCAT(pe.givenname, ' '), pe.familyname) as fullname, shp.role
                 FROM person pe
                 INNER JOIN session_has_person shp ON shp.personid = pe.personid
                 INNER JOIN blsession s ON s.sessionid = shp.sessionid
                 INNER JOIN proposal p ON p.proposalid = s.proposalid
                 WHERE s.startdate > TO_DATE(:1,'YYYY') $where
-                ");
+                ", $args);
 
             $users = array();
             foreach ($user_tmp as $u) {
@@ -87,12 +77,12 @@
                 $en = strtotime($v['E'].' '.$v['EN']);
                 
                 $title = $v['VIS'].($v['LC'] ? ' LC: '.$v['LC'] : '');
-                if (!in_array($arg, $mx_beamlines)) $title = $v['BL'].': '.$title;
+                if (!$hash['BEAMLINE']) $title = $v['BL'].': '.$title;
                 
                 $us = '';
                 if (array_key_exists($v['SESSIONID'], $users)) {
                     foreach ($users[$v['SESSIONID']] as $u) {
-                        $us .= 'ATTENDEE;CN="'.$u['FULLNAME']."\":MAILTO:".$u['NAME']."\r\n";
+                        $us .= 'ATTENDEE;CN="'.$u['FULLNAME']."\":MAILTO:".$u['ROLE']."\r\n";
                     }
                 }
                 
@@ -100,7 +90,7 @@
                 $output .= "BEGIN:VEVENT\r\nDTSTAMP:".date('Ymd\THi',$st)."00Z\r\nDTSTART:".date('Ymd\THi', $st)."00Z\r\nDTEND:".date('Ymd\THi', $en)."00Z\r\nSUMMARY:".$title."\r\n".$us."\r\nEND:VEVENT\r\n";
             }
             
-            // $this->app->contentType("text/calendar; charset=utf-8");
+            $this->app->contentType("text/calendar; charset=utf-8");
             $this->app->response()->body("BEGIN:VCALENDAR\r\nVERSION:2.0\r\n$output\r\nEND:VCALENDAR\r\n");
         }
         
