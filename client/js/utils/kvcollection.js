429d266cc8b5a42c1cdca80a8c45ab95a50a9205
Stu Fisher <stu@q3f.org>
2016-02-16 10:28:33 +0100

    kv collection can return empty value

--- client/js/utils/kvcollection.js
+++ client/js/utils/kvcollection.js
@@ -1,15 +1,19 @@
 define(['backbone'], function(Backbone) {
     
     return {
-        opts: function() {
-            return this.map(function(m) { return '<option value="'+m.get(this.valueAttribute)+'">'+m.get(this.keyAttribute)+'</option>' }, this)
+        opts: function(options) {
+            return (options && options.empty ? '<option value=""> - </option>' : '') +
+                this.map(function(m) { return '<option value="'+m.get(this.valueAttribute)+'">'+m.get(this.keyAttribute)+'</option>' }, this)
         },
 
-        kv: function() {
+        kv: function(options) {
             var kv = {}
+            if (options && options.empty) kv[''] = ' - '
+                
             this.each(function(m) {
                 kv[m.get(this.valueAttribute)] = m.get(this.keyAttribute)
             }, this)
+
             return kv
         },
     }
