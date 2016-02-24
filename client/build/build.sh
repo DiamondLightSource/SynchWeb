e68ccda0ba8057f1aaf5f701b5bb4480c3b67f17
Stu Fisher <stu@q3f.org>
2016-02-24 11:47:52 +0100

    automatically grab latest git hash and use it to build client

--- client/build/build.sh
+++ client/build/build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-r.js -o build.js dir=../dist/$1
-rm ../dist/$1/config.json
-rm -rf ../dist/$1/templates
+HASH=`git log --pretty=format:'%h' -n 1`
+echo "Building into: $HASH"
+
+r.js -o build.js dir=../dist/$HASH
+rm ../dist/$HASH/config.json
+rm -rf ../dist/$HASH/templates
+echo "Linking config.json"
+cd ../dist/$HASH
+ln -s ../../js/config.json config.json
