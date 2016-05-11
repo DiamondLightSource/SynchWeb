16bcc7b05d3a3f6036ae1c654a4c73a137481d84
Stu Fisher <stu@q3f.org>
2016-05-11 09:45:02 +0200

    update CAS authentication handler

--- api/includes/class.auth-cas.php
+++ api/includes/class.auth-cas.php
@@ -4,19 +4,21 @@ require_once('config.php');
 
 class CASAuthentication extends AuthenticationBase implements Authentication {
 
-	function authenticate($login, $password) {
-		global $cas_url;
-		return true;
+    function authenticate($login, $password) {
+        global $cas_url;
 
-		$ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $cas_url.'/v1/tickets');
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $cas_url.'/cas/v1/tickets');
         curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS,"username=$login&password=$password");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         $response = curl_exec($ch);
         $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         curl_close($ch);
 
-        return $code == 200;
-	}
+        // CAS returns 201 = Created
+        return $code == 201;
+    }
 
 }
