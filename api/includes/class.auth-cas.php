a930d10089a80df31fd877b1c75eb162facd47bd
stufisher <stu@q3f.org>
2016-05-18 15:05:50 +0200

    encode url params...

--- api/includes/class.auth-cas.php
+++ api/includes/class.auth-cas.php
@@ -7,13 +7,18 @@ class CASAuthentication extends AuthenticationBase implements Authentication {
     function authenticate($login, $password) {
         global $cas_url;
 
+        $fields = array(
+            'username' => $login,
+            'password' => $password,
+        );
+
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $cas_url.'/cas/v1/tickets');
         curl_setopt($ch, CURLOPT_HEADER, 0);
         curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS,"username=$login&password=$password");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($fields));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        $response = curl_exec($ch);
+        $this->response = curl_exec($ch);
         $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         curl_close($ch);
 
