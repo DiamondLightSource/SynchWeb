344bbdf0fa252cea674eecd8ae65b1ddd5e67373
stufisher <stu@q3f.org>
2016-05-20 10:40:53 +0200

    allow app to check for existing SSO

--- client/js/views/login.js
+++ client/js/views/login.js
@@ -31,18 +31,49 @@ define(['views/form',
             this.model = new Login()
         },
 
+        onRender: function() {
+            if (!app.token) this.checkExistingSession()
+        },
+
+        // Hack for CAS SSO
+        checkExistingSession: function() {
+            var self = this
+            var fr = $('iframe')
+            fr.load(function() {
+                var content = fr.contents().text()
+                if (content) {
+                    try {
+                        var response = JSON.parse(content)
+                        if ('jwt' in response) {
+                            app.alert({ message: 'Found a valid session, auto-logging you in' })
+                            self.$el.find('form').addClass('loading')
+                            setTimeout(function() {
+                                self.setToken(response)
+                            }, 2000)
+                        }
+                    } catch(err) {
+                        // Not valid JSON
+                    }
+                }
+            })
+            fr.attr('src',app.apiurl+'/authenticate/check')
+        },
+
         onShow: function() {
             if (app.token) {
                 console.log('invald token alert')
                 app.alert({ message: 'Your session has expired, please log in again', persist: 'login' })
-                // app.token = null
             }
         },
         
         success: function(model, response, options) {
+            this.setToken(response)
+        },
+
+        setToken: function(response) {
             app.token = response.jwt
+
             sessionStorage.setItem('token', app.token)
-            console.log('success from login', app.token)
             this.$el.find('form').addClass('loading')
             app.getuser({
                 callback: function() {
