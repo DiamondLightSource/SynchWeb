d3cf2ac14b9a3eb73b9b60b81e412106374be22c
stufisher <stu@q3f.org>
2016-05-23 10:44:38 +0200

    add powder as an api type

---
+++ api/includes/class.type.pow.php
@@ -0,0 +1,10 @@
+<?php
+
+    require_once('includes/class.type.gen.php');
+    class POW extends GEN {
+
+    	var $staff_permission = 'powder_admin';
+    	
+    }
+
+?>
---
+++ client/js/modules/types/pow/menu.js
@@ -0,0 +1,24 @@
+define([], function() {
+    
+    return {
+        menu: {
+            dc: 'View All Data',
+            visits: 'Visits',
+            calendar: 'Calendar',
+            shipments: 'Shipments',
+            samples: 'Samples',
+            proteins: 'Components',
+            contacts: 'Lab Contacts',
+            stats: 'Statistics',
+        },
+        
+        extra: {
+            //projects: 'Projects',
+        },
+        
+        admin: {
+            faults: { title: 'Fault Reports', icon: 'tasks' },
+        },
+    }
+    
+})
