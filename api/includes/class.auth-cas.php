2abc427d4fecd7afe400723fddbd005124a2394e
stufisher <stu@q3f.org>
2016-06-21 15:21:03 +0200

    kill the session cookie from phpCAS

--- api/includes/class.auth-cas.php
+++ api/includes/class.auth-cas.php
@@ -18,6 +18,8 @@ class CASAuthentication extends AuthenticationBase implements Authentication {
             $check = phpCAS::checkAuthentication();
 
             // phpCAS will now lumber us with a session, which we dont want
+            $params = session_get_cookie_params();
+            setcookie(session_name(), '', 0, $params['path'], $params['domain'], $params['secure'], isset($params['httponly']));
             session_unset();
             session_destroy();
 
