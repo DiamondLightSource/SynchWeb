4d20443f241d9b08360ba6772f968db363397601
stufisher <stu@q3f.org>
2016-08-17 09:49:57 +0200

    make dewars server side pageable

--- client/js/collections/dewars.js
+++ client/js/collections/dewars.js
@@ -1,18 +1,31 @@
 define(['backbone.paginator', 'models/dewar', 'utils/kvcollection'], function(PagableCollection, Dewar, KVCollection) {
-       
-  return PagableCollection.extend(_.extend({}, KVCollection, {
-  	mode: 'client',
-    model: Dewar,
-    url: function() { return '/shipment/dewars'+(this.id ? '/sid/'+this.id : '')+(this.fc ? '/fc/'+this.fc : '') },
-      
-    keyAttribute: 'CODE',
-    valueAttribute: 'DEWARID',
 
-    initialize: function(models, options) {
-        if (options) {
-        	this.id = options.id
-        	this.fc = options.FACILITYCODE
-     	}
-    },      
+    return PagableCollection.extend(_.extend({}, KVCollection, {
+        mode: 'server',
+        model: Dewar,
+        url: function() { return '/shipment/dewars'+(this.id ? '/sid/'+this.id : '')+(this.fc ? '/fc/'+this.fc : '') },
+          
+        state: {
+            pageSize: 15,
+        },
+
+        keyAttribute: 'CODE',
+        valueAttribute: 'DEWARID',
+
+        initialize: function(models, options) {
+            if (options) {
+                this.id = options.id
+                this.fc = options.FACILITYCODE
+              }
+        }, 
+
+        parseState: function(r, q, state, options) {
+            return { totalRecords: r.total }
+        },
+            
+        parseRecords: function(r, options) {
+            return r.data
+        },     
   }))
+
 })
