66bbb7257c69a58515758cadc78803c74b78178e
stufisher <stu@q3f.org>
2016-10-21 11:31:19 +0200

    extend diffraction plan model

--- client/js/modules/imaging/models/plan.js
+++ client/js/modules/imaging/models/plan.js
@@ -30,11 +30,50 @@ define(['backbone'], function(Backbone) {
                 pattern: 'number',
             },
 
-            PREFERREDBEAMSIZEX: {
+            PREFERREDBEAMSIZEY: {
                 required: true,
                 pattern: 'number',
             },
 
+            BOXSIZEX: {
+                required: true,
+                pattern: 'digits',
+            },
+
+            BOXSIZEY: {
+                required: true,
+                pattern: 'digits',
+            },
+
+            AXISRANGE: {
+                required: true,
+                pattern: 'number',
+            },
+
+            AXISSTART: {
+                required: true,
+                pattern: 'number',
+            },
+
+            NUMBEROFIMAGES: {
+                required: true,
+                pattern: 'digits',
+            },
+
+            TRANSMISSION: {
+                required: true,
+                pattern: 'number',
+            },
+
+            WAVELENGTH: {
+                required: true,
+                pattern: 'digits',
+            },
+
+            MONOCHROMATOR: {
+                required: true,
+                pattern: 'word',
+            },
         },
       
     })
