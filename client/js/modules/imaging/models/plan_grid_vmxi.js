316c6c641e82fb2266ad762acd24e553e9f2bd18
stufisher <stu@q3f.org>
2016-10-24 12:25:27 +0100

    add beamline specific exp plan models

---
+++ client/js/modules/imaging/models/plan_grid_vmxi.js
@@ -0,0 +1,57 @@
+define(['backbone'], function(Backbone) {
+    
+    return Backbone.Model.extend({
+        idAttribute: 'DIFFRACTIONPLANID',
+        urlRoot: '/sample/plan',
+
+        validation: {
+            EXPERIMENTKIND: {
+                required: true,
+                pattern: 'word',
+            },
+
+            REQUIREDRESOLUTION: {
+                required: true,
+                pattern: 'number',
+            },
+
+            EXPOSURETIME: {
+                required: true,
+                pattern: 'number',
+            },
+
+            BOXSIZEX: {
+                required: true,
+                pattern: 'digits',
+                range: [5,100],
+            },
+
+            BOXSIZEY: {
+                required: true,
+                pattern: 'digits',
+                range: [5,100],
+            },
+
+            AXISRANGE: {
+                required: false,
+                pattern: 'number',
+                range: [0,10],
+            },
+
+            TRANSMISSION: {
+                required: true,
+                pattern: 'number',
+                range: [0,100],
+            },
+
+            ENERGY: {
+                required: true,
+                pattern: 'digits',
+                range: [12658,12658]
+            },
+
+        },
+      
+    })
+       
+})
---
+++ client/js/modules/imaging/models/plan_xfe_vmxi.js
@@ -0,0 +1,52 @@
+define(['backbone'], function(Backbone) {
+    
+    return Backbone.Model.extend({
+        idAttribute: 'DIFFRACTIONPLANID',
+        urlRoot: '/sample/plan',
+
+        validation: {
+            EXPERIMENTKIND: {
+                required: true,
+                pattern: 'word',
+            },
+
+            PREFERREDBEAMSIZEX: {
+                required: true,
+                pattern: 'digits',
+                range: [2,30],
+            },
+
+            PREFERREDBEAMSIZEY: {
+                required: true,
+                pattern: 'digits',
+                range: [2,30],
+            },
+
+            AXISRANGE: {
+                required: false,
+                pattern: 'number',
+                range: [0,10],
+            },
+
+            EXPOSURETIME: {
+                required: true,
+                pattern: 'number',
+            },
+
+            TRANSMISSION: {
+                required: true,
+                pattern: 'number',
+                range: [0,100],
+            },
+
+            ENERGY: {
+                required: true,
+                pattern: 'digits',
+                range: [12658,12658]
+            },
+
+        },
+      
+    })
+       
+})
