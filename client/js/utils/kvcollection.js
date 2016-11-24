15cf571b1dc4e773a15450b058872e976c859a3f
stufisher <stu@q3f.org>
2016-11-24 16:24:09 +0100

    kvcol can add a class to an option

--- client/js/utils/kvcollection.js
+++ client/js/utils/kvcollection.js
@@ -3,7 +3,13 @@ define(['backbone'], function(Backbone) {
     return {
         opts: function(options) {
             return (options && options.empty ? '<option value=""> - </option>' : '') +
-                this.map(function(m) { return '<option value="'+m.get(this.valueAttribute)+'">'+m.get(this.keyAttribute)+'</option>' }, this)
+                this.map(function(m) { 
+                    var cl= ''
+                    if (options && options.addClass && options.classProperty) {
+                        if (m.get(options.classProperty) == options.classPropertyValue) cl = options.addClass
+                    }
+                    return '<option class="'+cl+'" value="'+m.get(this.valueAttribute)+'">'+m.get(this.keyAttribute)+'</option>' 
+                }, this)
         },
 
         kv: function(options) {
