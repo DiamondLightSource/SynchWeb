f4af6b6103168b859423ba72f745b79bba993603
stufisher <stu@q3f.org>
2016-11-25 13:27:21 +0100

    add barcode validator

--- client/js/models/container.js
+++ client/js/models/container.js
@@ -1,9 +1,14 @@
 define(['backbone'], function(Backbone) {
     
-    return Backbone.Model.extend({
+    var Container = Backbone.Model.extend({
         idAttribute: 'CONTAINERID',
         urlRoot: '/shipment/containers',
+        _cache: {},
         
+        defaults: {
+            new: false,
+        },
+
         validation: {
             NAME: {
                 required: true,
@@ -29,8 +34,48 @@ define(['backbone'], function(Backbone) {
                 required: false,
                 pattern: 'number',
             },
+
+            // BARCODE: {
+            //     pattern: 'wwdash',
+            //     required: function() {
+            //         return this.get('REQUESTEDIMAGERID') != '' || this.get('CONTAINERTYPE') == 'PCRStrip'
+            //     }
+            // },
+
+            BARCODE: _.debounce(function(value, attr, state) {
+                if ((this.get('REQUESTEDIMAGERID') != '' || this.get('CONTAINERTYPE') == 'PCRStrip') && !value) {
+                    return Backbone.Validation.messages.required
+                } else if (!value) return
+
+                if (!Backbone.Validation.patterns.wwdash.test(value)) {
+                    return Backbone.Validation.messages.wwdash
+                }
+
+                if (!(value in this._cache)) {
+                    var self = this
+                    Backbone.ajax({
+                        url: app.apiurl+'/shipment/containers/barcode/'+value,
+                        async: false,
+                        success: function(resp) {
+                            console.log('container', resp)
+                            self._cache[value] = resp.PROP
+                        },
+
+                        failure: function() {
+                            self._cache[value] = false
+                        }
+                    })
+                }
+
+                if (this._cache[value]) {
+                    return 'That barcode is already registered to '+this._cache[value]
+                }
+            }, 200),
+
         },
         
     })
+
+    return Container
     
 })
