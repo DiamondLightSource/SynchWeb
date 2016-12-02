e32bb7ba733cd8ab2361b8c7e9ff91567d17da9c
Karl Levik <karl.levik@diamond.ac.uk>
2016-12-02 23:39:18 +0000

    Logging (syslog/Graylog) instead of prints + robustification by wrapping critical parts in try ... except

--- api/formulatrix/uploader/formulatrix_uploader.py
+++ api/formulatrix/uploader/formulatrix_uploader.py
@@ -24,8 +24,13 @@ import time
 import glob
 import re
 import os
+import sys
+import atexit
+import signal
 import errno
 import subprocess
+import logging
+import logging.handlers
 import MySQLdb
 from PIL import Image
 from shutil import copyfile
@@ -82,12 +87,12 @@ class FormulatrixUploader:
         while self._running:
             files = glob.glob(self.config['holding_dir']+"/*EF*.xml")
             for xml in files:
-                print xml
+                logging.getLogger().debug(xml)
                 st = os.stat(xml)
                 
                 image = xml.replace('.xml', '.jpg')
                 if not os.path.exists(image):
-                    print 'Corresponding image not found for', xml, 'expected', image
+                    logging.getLogger().error('Corresponding image not found for %s expected %s' % (str(xml), str(image)) )
                     continue
 
 
@@ -101,7 +106,7 @@ class FormulatrixUploader:
 
                     inspectionid = re.sub('\-.*', '', root.find('oppf:ImagingId', nss).text)
 
-                    print 'inspection', inspectionid
+                    logging.getLogger().debug('inspection: %s' % str(inspectionid))
 
                     container = self._get_container(inspectionid)
                     if container is None:
@@ -110,7 +115,7 @@ class FormulatrixUploader:
                     # Check if the visit dir exists yet
                     new_root = '{root}/{year}/{visit}'.format(root=self.config['upload_dir'], year=container['year'], visit=container['visit'])
                     if not os.path.exists(new_root):
-                        print 'Visit location for image doesnt exist', new_root
+                        logging.getLogger().error('Visit location for image doesnt exist: %s' % new_root)
                         continue
 
                     # Keep images in visit/imaging/containerid/inspectionid
@@ -129,10 +134,10 @@ class FormulatrixUploader:
 
                     position = self._get_position(root.find('oppf:Drop', nss).text, container['containertype'])
                     if position is None:
-                        print 'Could not match drop to position', root.find('oppf:Drop', nss).text, container['containertype']
+                        logging.getLogger().error('Could not match drop: %s to position: %s' % (root.find('oppf:Drop', nss).text, container['containertype']) )
                         continue
 
-                    print 'Drop', root.find('oppf:Drop', nss).text, 'position', position
+                    logging.getLogger().debug('Drop: %s position: %s' % (root.find('oppf:Drop', nss).text, position)) 
 
                     sampleid = self._get_sampleid(position, container['containerid'])
                     if sampleid is None:
@@ -144,7 +149,9 @@ class FormulatrixUploader:
 
                     db.pq("""INSERT INTO BLSampleImage (blsampleid, micronsperpixelx, micronsperpixely, containerinspectionid)
                         VALUES (%s,%s,%s,%s)""", [sampleid, mppx, mppy, inspectionid])
-                    print "INSERT INTO BLSampleImage (blsampleid, micronsperpixelx, micronsperpixely, containerinspectionid) VALUES (%s,%s,%s,%s)", [sampleid, mppx, mppy, inspectionid]
+                    logging.getLogger().debug("INSERT INTO BLSampleImage "\
+                                              "(blsampleid, micronsperpixelx, micronsperpixely, containerinspectionid) "\
+                                              "VALUES (%s,%s,%s,%s)" % (str(sampleid), str(mppx), str(mppy), str(inspectionid)))
 
                     iid = db.id()
 
@@ -152,33 +159,42 @@ class FormulatrixUploader:
                     new_file = '{path}/{iid}.jpg'.format(path=new_path, iid=iid)
 
                     db.pq("""UPDATE BLSampleImage set imagefullpath=%s WHERE blsampleimageid=%s""", [new_file, iid])
-                    print "UPDATE BLSampleImage set imagefullpath=%s WHERE blsampleimageid=%s", [new_file, iid]
+                    logging.getLogger().debug("UPDATE BLSampleImage set imagefullpath=%s WHERE blsampleimageid=%s" % (new_file, str(iid)))
 
                     # move image
-                    print 'copy', image, new_file
-                    copyfile(image, new_file)
+                    logging.getLogger().debug('copy: %s to %s' % (image, new_file))
+                    try:
+                        copyfile(image, new_file)
+                    
+                        # create a thumbnail
+                        file, ext = os.path.splitext(new_file)
+                        try:
+                            im = Image.open(new_file)
+                            im.thumbnail((config['thumb_width'], config['thumb_height']))
+                            try:
+                                im.save(file+'th'+ext)
 
-                    # create a thumbnail
-                    file, ext = os.path.splitext(new_file)
-                    im = Image.open(new_file)
-                    im.thumbnail((config['thumb_width'], config['thumb_height']))
-                    im.save(file+'th'+ext)
+                                # clear up
+                                self._move_files(image, xml, 'processed')
 
-                    # clear up
-                    self._move_files(image, xml, 'processed')
+                                #os.unlink(image)
+                                #os.unlink(xml)
 
-                    #os.unlink(image)
-                    #os.unlink(xml)
+                            except IOError as e:
+                                logging.getLogger().error('Error saving image file %s' % file+'th'+ext)
+                        except IOError as e:
+                            logging.getLogger().error('Error opening image file %s' % new_file)
+                    except IOError as e:
+                        logging.getLogger().error('Error copying image file %s to %s' % (image, new_file))
 
-
-            print 'Sleeping until next iteration'
+            logging.getLogger().debug('Sleeping until next iteration')
             time.sleep(30)
 
 
     def _move_files(self, image, xml, path):
         for f in [image, xml]:
             os.rename(f, f.replace(self.config['holding_dir'], self.config['holding_dir']+'/'+path))
-            print 'move', f, f.replace(self.config['holding_dir'], self.config['holding_dir']+'/'+path)
+            logging.getLogger().debug('move %s %s' % (f, f.replace(self.config['holding_dir'], self.config['holding_dir']+'/'+path)))
 
 
     def _get_container(self, inspectionid):
@@ -193,17 +209,16 @@ class FormulatrixUploader:
             LIMIT 1""", [inspectionid])
 
         if not len(container):
-            print 'Couldnt find container for inspectionid', inspectionid
+            logging.getLogger().error('Couldnt find container for inspectionid %s' % str(inspectionid))
             return
 
-        print container
+        logging.getLogger().debug(str(container))
 
         if not container[0]['sessionid']:
-            print 'Container has no sessionid', container[0]['containerid'], inspectionid
+            logging.getLogger().error('Container %s has no sessionid. inspectionid is %s ' % (str(container[0]['containerid']), str(inspectionid)))
             return
 
         return container[0]
-
 
 
     def _get_position(self, text_position, platetype):
@@ -217,14 +232,13 @@ class FormulatrixUploader:
         # This should be in the database, currently in json format embedded in this collection:
         # http://ispyb.diamond.ac.uk/beta/client/js/modules/shipment/collections/platetypes.js
         if not platetype in self.config['types']:
-            print 'Unknown plate type', ty
+            logging.getLogger().error('Unknown plate type: %s' % platetype)
             return
 
         ty = self.config['types'][platetype]
 
         # Position is a linear sequence left to right across the plate
         return (ty['well_per_row']*row*ty['drops_per_well']) + (col*ty['drops_per_well']) + (drop-1) + 1
-
 
 
     # Return a blsampleid from a position and containerid
@@ -236,20 +250,54 @@ class FormulatrixUploader:
             LIMIT 1""", [position, containerid])
 
         if not len(sample):
-            print 'Couldnt find a blsample for containerid', containerid, 'position', position
+            logging.getLogger().error('Couldnt find a blsample for containerid: %s, position: %s', str(containerid), str(position))
             return
 
-        print sample[0]
+        logging.getLogger().debug(str(sample[0]))
 
         return sample[0]['blsampleid']
 
+
+def killHandler(sig,frame):
+    hostname = os.uname()[1]
+    logging.getLogger().warning("%s: got SIGTERM on %s :-O" % (sys.argv[0], hostname))
+    logging.shutdown()
+    os._exit(-1)
+
+def set_logging(logs):
+    levels_dict = {"debug" : logging.DEBUG, "info" : logging.INFO, "warning" : logging.WARNING, "error" : logging.ERROR, "critical" : logging.CRITICAL}
+
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    for log_name in logs:
+        handler = None
+        if log_name == "syslog":
+            handler = logging.handlers.SysLogHandler(address=(logs[log_name]['host'], logs[log_name]['port']))
+        elif log_name == "rotating_file":
+            handler = logging.handlers.RotatingFileHandler(filename=logs[log_name]['filename'], maxBytes=logs[log_name]['max_bytes'], backupCount=logs[log_name]['no_files'])
+        else:
+            sys.exit("Invalid logging mechanism defined in config: %s. (Valid options are syslog and rotating_file.)" % log_name)
+        
+        handler.setFormatter(logging.Formatter(logs[log_name]['format']))
+        level = logs[log_name]['level']
+        if levels_dict[level]:
+            handler.setLevel(levels_dict[level])
+        else:
+            handler.setLevel(logging.WARNING)
+        logger.addHandler(handler)
 
 
 cf = open('config.json')
 config = json.load(cf)
 cf.close()
+ 
+set_logging(config['logging'])
+
+atexit.register(logging.shutdown)
+signal.signal(signal.SIGTERM,killHandler)
 
 db = MySQL(user=config['user'], pw=config['pw'], db=config['db'], host=config['host'])
 
 uploader = FormulatrixUploader(db=db, config=config)
 uploader.run()
+
