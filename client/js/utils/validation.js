0b516b52fd488123ad5e62f8aca2e78c3b7f9648
stufisher <stu@q3f.org>
2017-05-01 18:11:20 +0200

    correct dewar code validator

--- client/js/utils/validation.js
+++ client/js/utils/validation.js
@@ -20,7 +20,7 @@ define(['backbone', 'backbone-validation'], function(Backbone) {
         datetime: 'Please specify a valid date and time',
         edate: 'Please specify a valid date (european style)',
         word: 'This field must contain only letters and numbers',
-        fcode: 'This field must be of the format DLS-MX-XXXX',
+        fcode: 'This field must be of the format DLS-XX-XXXX',
         datetime: 'This field must be of the format DD-MM-YYYY HH:MM',
         sequence: 'This field may only contain word characters and line returns',
         address: 'This field may only contain word character, spaces, and line returns',
