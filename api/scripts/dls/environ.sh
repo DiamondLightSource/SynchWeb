84dd564e8aa81739ecb3e562a71ca3ff63517295
stufisher <stu@q3f.org>
2017-05-09 08:47:37 +0200

    reprocessing abstraction

---
+++ api/scripts/dls/environ.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# CCP4
+
+. /dls_sw/apps/ccp4/64/7.0/update35/ccp4-7.0/bin/ccp4.setup-sh
+
+# XDS
+
+export PATH=/dls_sw/apps/XDS/20170215/:${PATH}
+
+# DIALS
+
+. /dls_sw/apps/dials/dials-v1-5-1/build/setpaths.sh
+
