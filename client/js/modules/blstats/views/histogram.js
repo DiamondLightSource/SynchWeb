4c74e828cb0ac6e2a93830e6200de8ecd2811031
stufisher <stu@q3f.org>
2017-06-15 14:34:25 +0200

    make histogram zoomable, add tooltip, rationalise ticks

--- client/js/modules/blstats/views/histogram.js
+++ client/js/modules/blstats/views/histogram.js
@@ -7,7 +7,30 @@ define(['marionette', 'modules/blstats/models/histogram', 'utils',
     
     return Marionette.ItemView.extend({
         template: _.template('<div id="visit_pie"></div><p><%=title%></p>'),
-                                               
+                              
+        events: {
+            'plotselected': 'zoom',
+            'dblclick': 'reset',
+        },
+
+        zoom: function(e, ranges) {
+                var opts = this.plot.getOptions()
+                opts.xaxes[0].min = ranges.xaxis.from
+                opts.xaxes[0].max = ranges.xaxis.to
+
+                this.plot.setupGrid()
+                this.plot.draw()
+                this.plot.clearSelection()
+        },
+
+        reset: function(e) {
+            this.zoom(e, {
+                xaxis: {
+                    from: null, to: null
+                },
+            })
+        },
+
         templateHelpers: function() {
             return {
                 title: this.getOption('title')
@@ -19,6 +42,13 @@ define(['marionette', 'modules/blstats/models/histogram', 'utils',
             this.collection.listenTo(this.collection, 'change:data', this.render)
         },
         
+
+        getToolTip: function(lab, x, y, item) {
+            console.log(lab, x, y, item)
+            var f = this.collection.first()
+            return lab+': '+Object.keys(f.get('data'))[x]
+        },
+
           
         onRender: function() {
             var options = {
@@ -31,7 +61,13 @@ define(['marionette', 'modules/blstats/models/histogram', 'utils',
                 },
                 grid: {
                   borderWidth: 0,
+                  hoverable: true,
                 },
+
+                selection: { mode: 'x' },
+                tooltipOpts: { content: this.getToolTip.bind(this) },
+                tooltip: true,
+
                 yaxes: [{}, { position: 'right' }],
             }
             
@@ -39,8 +75,9 @@ define(['marionette', 'modules/blstats/models/histogram', 'utils',
             if (!f.get('data')) return
             var ticks = []
             var i = 0
+            var int = Math.floor(Object.keys(f.get('data')).length/15)
             _.each(f.get('data'), function(v, bin) {
-                ticks.push([i, i % 2 == 0 ? bin : ''])
+                ticks.push([i, i % int == 0 ? bin : ''])
                 i++
             })  
             options.xaxis.ticks = ticks
@@ -57,9 +94,8 @@ define(['marionette', 'modules/blstats/models/histogram', 'utils',
                 data.push(series)
             })
 
-            console.log(data)
             if (data.length) {
-                $.plot(this.$el.find('#visit_pie'), data, options)
+                this.plot = $.plot(this.$el.find('#visit_pie'), data, options)
                 this.$el.css('opacity', 1)
             }
         },
