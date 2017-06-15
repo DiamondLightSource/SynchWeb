045747cd5e349f966c405c803ae836b7bb650731
stufisher <stu@q3f.org>
2017-06-15 16:35:38 +0200

    add colour shader

--- client/js/utils.js
+++ client/js/utils.js
@@ -151,7 +151,12 @@ define(['marionette', 'views/dialog', 'jquery', 'jquery.color'], function(Marion
             return $.Color({ hue: (i*360/len), saturation: 0.90, lightness: 0.40, alpha: 1 }).toHexString();
         })
     },
-      
+    
+    shadeColor: function(color, percent) {   
+        var f=parseInt(color.slice(1),16),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
+        return "#"+(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
+    },
+
       
     // Check if an element is in view
     inView: function(element, threshold) {
