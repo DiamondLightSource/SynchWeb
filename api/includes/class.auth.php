4b4d8bcdda9921eadb6b5f9d531df783e49f8c18
stufisher <stu@q3f.org>
2017-06-15 11:34:02 +0200

    dont send empty header

--- api/includes/class.auth.php
+++ api/includes/class.auth.php
@@ -173,7 +173,7 @@
 			$this->app->response->setStatus($code);
 
 			// Cant call $app->halt as app not yet running, just end process
-			header(':', true, $code);
+			header('X-PHP-Response-Code: '.$code, true, $code);
        		header('Content-Type: application/json');
 			print json_encode($message);
 			exit();
