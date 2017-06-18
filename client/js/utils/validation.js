c450fefa7857b513b761504bcec39318b5c3b3d0
stufisher <stu@q3f.org>
2017-06-18 13:35:10 +0200

    add time validator

--- client/js/utils/validation.js
+++ client/js/utils/validation.js
@@ -8,24 +8,24 @@ define(['backbone', 'backbone-validation'], function(Backbone) {
         edate: /^\d\d-\d\d-\d\d\d\d$/,
         word: /^\w+$/,
         fcode: /^DLS\-(MX|IN)\-\d\d\d\d$/i,
-        datetime: /\d+-\d+-\d+ \d+:\d+/,
+        time: /^\d\d+:\d\$/,
         sequence: /^[>;\s\w+\n\(\)\.\|]+$/,
         address: /^(\w|\s|\-|\n)+$/,
         array: /^[\d+(.\d+)?),]+$/,
-    });
+    })
     
     _.extend(Backbone.Validation.messages, {
         required: 'This field is required',
         wwsdash: 'This field must contain only letters, numbers, spaces, underscores, and dashes',
         wwdash: 'This field must contain only letters, numbers, underscores, and dashes',
-        datetime: 'Please specify a valid date and time',
         edate: 'Please specify a valid date (european style)',
         word: 'This field must contain only letters and numbers',
         fcode: 'This field must be of the format DLS-XX-XXXX',
         datetime: 'This field must be of the format DD-MM-YYYY HH:MM',
+        time: 'This field must be of the format HH:MM',
         sequence: 'This field may only contain word characters and line returns',
         address: 'This field may only contain word character, spaces, and line returns',
         array: 'This field may only contain numbers and commas',
-    });
+    })
 
 })
