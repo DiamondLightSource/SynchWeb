4ecdc5063f4b6d8faa01ffe2ddfe0f10f0a278f1
stufisher <stu@q3f.org>
2017-06-21 19:42:54 +0200

    add array shuffle

--- client/js/utils.js
+++ client/js/utils.js
@@ -199,6 +199,19 @@ define(['marionette', 'views/dialog', 'jquery', 'jquery.color'], function(Marion
                 window.location = url+'?token='+resp.token
             }
         })
+    },
+
+    shuffle: function(array) {
+        var currentIndex = array.length, temporaryValue, randomIndex
+        while (0 !== currentIndex) {
+            randomIndex = Math.floor(Math.random() * currentIndex)
+            currentIndex -= 1
+            temporaryValue = array[currentIndex]
+            array[currentIndex] = array[randomIndex]
+            array[randomIndex] = temporaryValue
+        }
+
+        return array
     }
     
   }
