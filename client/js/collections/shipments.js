da9bca717afbc29bc098fa77f04494ebec763424
stufisher <stu@q3f.org>
2017-06-28 12:07:46 +0200

    shipments is now server side managed

--- client/js/collections/shipments.js
+++ client/js/collections/shipments.js
@@ -1,16 +1,24 @@
 define(['backbone.paginator', 'models/shipment', 'utils/kvcollection'], function(PageableCollection, Shipment, KVCollection) {
        
-  return PageableCollection.extend(_.extend({}, KVCollection, {
-    model: Shipment,
-    mode: 'client',
-    url: '/shipment/shipments',
-                                  
-    keyAttribute: 'SHIPPINGNAME',
-    valueAttribute: 'SHIPPINGID',
+    return PageableCollection.extend(_.extend({}, KVCollection, {
+        model: Shipment,
+        mode: 'server',
+        url: '/shipment/shipments',
+                                      
+        keyAttribute: 'SHIPPINGNAME',
+        valueAttribute: 'SHIPPINGID',
 
-    state: {
-      pageSize: 15,
-    },
+        state: {
+            pageSize: 15,
+        },
+
+        parseState: function(r, q, state, options) {
+            return { totalRecords: r.total }
+        },
+            
+        parseRecords: function(r, options) {
+            return r.data
+        },
       
-  }))
+    }))
 })
