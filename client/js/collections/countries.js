a1f3551c9406ff92e9eeb2d25655678df53aaf70
stufisher <stu@q3f.org>
2017-07-07 10:10:34 +0200

    sort uk to top

--- client/js/collections/countries.js
+++ client/js/collections/countries.js
@@ -8,7 +8,12 @@ define(['backbone.paginator', 'utils/kvcollection'], function(PageableCollection
             pageSize: 15,
         },
 
-        comparator: 'TITLE',
+        // comparator: 'TITLE',
+        comparator: function(m1,m2) {
+            if (m1.get('TITLE') == 'United Kingdom') return -1
+            else if (m2.get('TITLE') == 'United Kingdom') return 1
+            else return m1.get('TITLE') == m2.get('TITLE') ? 0 : (m1.get('TITLE') > m2.get('TITLE') ? 1 : -1)
+        },
 
         keyAttribute: 'TITLE',
         valueAttribute: 'TITLE',
