5651d572d792df4f3390e5bd1933d3b7bbbee516
stufisher <stu@q3f.org>
2017-08-14 14:52:32 +0200

    update em dc view

--- client/js/modules/types/em/dc/dc.js
+++ client/js/modules/types/em/dc/dc.js
@@ -1,13 +1,97 @@
 define([
     'modules/types/gen/dc/dc',
-    'modules/types/em/dc/views/drift',
     'modules/types/em/dc/views/apstatusitem',
-    'tpl!templates/types/em/dc/dc.html'], function(DCItemView, DriftPlot, APStatusItem, template) {
+    'modules/types/em/dc/views/overview',
+    'modules/types/em/dc/views/micrograph',
+    'modules/types/em/dc/views/autoprocessing',
+    'tpl!templates/types/em/dc/dc.html'], function(DCItemView, 
+        APStatusItem, Overview, Micrograph, 
+        EMAutoProcessingView, 
+        template) {
 
-    return DCItemView.extend({
+    var EMDCView = DCItemView.extend({
         template: template,
-        plotView: DriftPlot,
+        plotView: null,
         apStatusItem: APStatusItem,
+
+        ui: {
+            temp: 'span.temp',
+            exp: 'i.expand',
+            cc: '.dcc',
+            rp: 'a.reprocess',
+        },
+
+        regions: {
+            ov: '.ov',
+            mg: '.mg',
+        },
+
+
+        events: {
+            'click .holder h1.ap': 'loadAP',
+            'click .holder h1.dp': 'loadDP',
+            'click .atp': 'addToProject',
+            'click .flag': 'flag',
+            'click .comments': 'showComments',
+            'click li.sample a': 'setProposal',
+            'click @ui.exp': 'expandPath',
+            'click a.attach': 'attachments',
+        },
+
+        initialize: function(options) {
+            EMDCView.__super__.initialize.apply(this,options)
+
+            this.m = 9.10938356e-31
+            this.h = 6.62607004e-34
+            this.c = 2.99792458e8
+            this.e = 1.60217662e-19
+
+            this.model.set('KV', this.l2kV(this.model.get('WAVELENGTH')))
+            this.overview = new Overview({ pm: this.model, imagestatuses: this.getOption('imagestatuses'), apstatus: this.getOption('apstatuses') })
+            this.micrograph = new Micrograph({ pm: this.model, apstatuses: this.getOption('apstatuses') })
+            this.listenTo(this.overview, 'current', this.loadPoint, this)
+        },
+
+        onShow: function() {
+            EMDCView.__super__.onShow.apply(this)
+            this.mg.show(this.micrograph)
+            this.ov.show(this.overview)
+        },
+
+
+        onDomRefresh: function() {
+            this.micrograph.triggerMethod('dom:refresh')
+            this.overview.triggerMethod('dom:refresh')
+        },  
+
+        loadPoint: function(point) {
+            this.micrograph.setMicrograph(point)
+            if (this.ap) {
+                this.ap.fetch(point)
+            }
+        },
+
+
+        loadAP: function(e) {
+            if (!this.ap) {
+                this.ap = new EMAutoProcessingView({ id: this.model.get('ID'), el: this.$el.find('div.autoproc') })
+            } else this.ap.$el.slideToggle()
+        },
+
+        // kV to lambda (in A)
+        kV2l: function(kV) {
+            var U = kV*1e3
+            return (this.h/Math.sqrt(2*this.m*this.e*U+(Math.pow(this.e*U/this.c,2))))*1e10
+        },
+
+
+        // lambda (in A) to kV
+        l2kV: function(l) {
+            return (( (-2*this.m*this.e) + Math.sqrt(Math.pow(2*this.m*this.e,2)+4*Math.pow(this.e*this.h/(this.c*l*1e-10),2)) ) / ( 2 * Math.pow(this.e/this.c,2) )/1000).toFixed(0)
+        },
+
     })
+
+    return EMDCView
 
 })
--- client/js/modules/types/em/dc/views/apstatusitem.js
+++ client/js/modules/types/em/dc/views/apstatusitem.js
@@ -1,30 +1,27 @@
-define(['modules/dc/views/apstatusitem'], function(APStatusItem, $) {
+define(['modules/dc/views/apstatusitem'], function(APStatusItem) {
 
     return APStatusItem.extend({
         
         onRender: function() {
-            var pcount;
-            if ('PARTICLES' in this.model.get('STATES')) {
-                var pcount = '('+this.model.get('STATES')['PARTICLES']+') '
-            }
+            var ps = []
+            _.each({'Motion Correction': 'MC', 'CTF': 'CTF'}, function(p,n) {
+                var tys = []
+                var cts = _.countBy(Object.values(this.model.get(p)), function(v) { return v })
+                _.each({
+                    null: '<i class="fa icon grey fa-cog fa-spin alt="Running"></i>',
+                    1: '<i class="fa icon green fa-check alt="Completed"></i>',
+                    0: '<i class="fa icon red fa-times alt="Failed"></i>'
+                }, function(icon, state) {
+                    if (cts[state]) tys.push(cts[state]+' '+icon)
+                }, this)
+
+                ps.push('<span>'+n+': '+tys.join(' ')+'</span>')
+            }, this)
           
-            this.ui.holder.eq(0).html('Auto Picker '+pcount+this.getIcon('pfinder'))
-            this.ui.holder.eq(1).html('2d Class '+this.getIcon('2dclass')+' 3d Class '+this.getIcon('3dclass'))
+            this.ui.holder.eq(0).html(ps.join(' '))
+            
         },
 
-
-        getIcon: function(key) {
-            var res = this.model.get('STATES')
-            var icon;
-            if (key in res) {
-                if (res[key] == null) icon = '<i class="fa icon grey fa-cog fa-spin alt="Running"></i>'
-                if (res[key] == 1) icon = '<i class="fa icon green fa-check alt="Completed"></i>'
-                if (res[key] == 0) icon = '<i class="fa icon red fa-times alt="Failed"></i>'
-
-            } else icon = '<i class="fa icon blue fa-question-circle alt="N/A"></i>'
-
-            return icon
-        }
     })
        
 })
