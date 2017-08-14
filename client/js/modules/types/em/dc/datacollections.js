edbb9f19234faa65743018048434017fa9b6faa6
stufisher <stu@q3f.org>
2017-08-14 14:51:37 +0200

    use new emapstatusitem

--- client/js/modules/types/em/dc/datacollections.js
+++ client/js/modules/types/em/dc/datacollections.js
@@ -2,11 +2,13 @@ define([
         'modules/types/gen/dc/datacollections',
         'modules/types/gen/dc/dclist',
         'modules/types/em/dc/dc',
+        'modules/types/em/collections/apstatuses',
         'tpl!templates/types/gen/dc/dclist.html',
         ],
-function(DataCollections, DCList, DCItemView, template) {
+function(DataCollections, DCList, DCItemView, EMAPStatuses, template) {
     
     var EMDCList = DCList.extend({
+        apStatusCollection: EMAPStatuses,
         dcViews: {
             data: DCItemView,
         },
