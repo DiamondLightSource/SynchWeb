080aa89cfcfb1ebaaed61b46d99cfd6a06a1d187
stufisher <stu@q3f.org>
2017-08-25 15:54:59 +0200

    add em visit stats

---
+++ client/js/modules/types/em/stats/views/visit.js
@@ -0,0 +1,94 @@
+define(['marionette',
+        'modules/fault/collections/faults',
+        'modules/fault/views/list',
+        'modules/stats/views/breakdown',
+        'modules/stats/views/details',
+        'modules/stats/views/pie',
+        'modules/stats/views/hourlies',
+        'modules/stats/views/ehc',
+
+        'modules/blstats/models/histogram',
+        'modules/blstats/views/histogram',
+    
+        'tpl!templates/types/em/stats/visit.html'], function(Marionette, Faults, FaultListView,
+        BreakdownView, DetailsView, PieView, HourliesView, EHCLogView,
+        Histogram, HistgramPlot,
+        template) {
+
+
+    var EMHistogram = Histogram.extend({
+        urlRoot: '/em/ctf/histogram',
+    })
+
+    var DriftHistogram = Histogram.extend({
+        urlRoot: '/em/mc/histogram',
+    })
+
+
+
+    return Marionette.LayoutView.extend({
+        template: template,
+        className: 'content',
+        
+        regions: {
+            bd: '.breakdown',
+            det: '.details',
+            call: '.callouts',
+            ehc: '.ehclogs',
+            hrs: '.hrs',
+            flt: '.faults',
+
+            rdefocus: '.defocus',
+            rastimatigm: '.astigmatism',
+            rresolution: '.resolution',
+            rdrift: '.drift',
+        },
+
+        initialize: function() {
+            this.defocus = new EMHistogram()
+            this.astigmatism = new EMHistogram()
+            this.resolution = new EMHistogram()
+            this.drift = new DriftHistogram()
+
+
+            this.defocus.fetch({
+                data: {
+                    visit: this.model.get('VISIT')
+                }
+            })
+
+            this.astigmatism.fetch({
+                data: {
+                    visit: this.model.get('VISIT'),
+                    ty: 'astigmatism',
+                }
+            })
+
+            this.resolution.fetch({
+                data: {
+                    visit: this.model.get('VISIT'),
+                    ty: 'resolution'
+                }
+            })
+        },
+        
+        
+        onShow: function() {
+            this.bd.show(new BreakdownView({ model: this.getOption('breakdown'), scatters: true }))
+            this.det.show(new DetailsView({ model: this.getOption('breakdown') }))
+            if (app.staff) this.ehc.show(new EHCLogView({ visit: this.model.get('VISIT') }))
+            
+            this.pie = new PieView({ visit: this.model.get('VISIT'), el: this.$el.find('#visit_pie') })
+            
+            this.faults = new Faults(null, { queryParams: { visit: this.model.get('VISIT') } })
+            this.faults.fetch()
+            
+            this.flt.show(new FaultListView({ collection: this.faults, filters: false, search: false }))
+
+            this.rdefocus.show(new HistgramPlot({ collection: new Backbone.Collection([this.defocus]) }))
+            this.rastimatigm.show(new HistgramPlot({ collection: new Backbone.Collection([this.astigmatism]) }))
+            this.rresolution.show(new HistgramPlot({ collection: new Backbone.Collection([this.resolution]) }))
+        },
+    })
+
+})
