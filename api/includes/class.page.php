21a3406fc71b1dd3182c7215234264b875c08919
stufisher <stu@q3f.org>
2017-09-01 19:24:08 +0200

    missing space

--- api/includes/class.page.php
+++ api/includes/class.page.php
@@ -432,7 +432,7 @@
         function pv($pvs,$full=false) {
             global $bl_pv_prog, $bl_pv_env;
             putenv($bl_pv_env);
-            exec($bl_pv_prog . implode(' ', $pvs) . ' 2>/dev/null', $ret);
+            exec($bl_pv_prog . ' ' . implode(' ', $pvs) . ' 2>/dev/null', $ret);
             $output = array();
             
             foreach ($ret as $i => $v) {
