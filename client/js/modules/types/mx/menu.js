90e62a754399f22a44776c480f1acc56ef220d0c
stufisher <stu@q3f.org>
2017-09-07 18:40:26 +0200

    correct permission...

--- client/js/modules/types/mx/menu.js
+++ client/js/modules/types/mx/menu.js
@@ -22,7 +22,7 @@ define([], function() {
         },
         
         admin: {
-            'stats/overview': { title: 'Reporting', icon: 'line-chart', permission: 'all_breakdown' },
+            'stats/overview/beamlines': { title: 'Reporting', icon: 'line-chart', permission: 'all_prop_stats' },
             'admin/imaging': { title: 'Imaging', icon: 'image', permission: 'imaging_dash' },
             'dewars/overview': { title: 'Logistics', icon: 'truck', permission: 'all_dewars' },
             statistics: { title: 'Stats', icon: 'pie-chart' },
