ee356513207c16a821977ca6398c6b31da91ef0a
stufisher <stu@q3f.org>
2017-11-17 13:47:55 +0100

    dont watch change

--- client/js/models/datacollectionplan.js
+++ client/js/models/datacollectionplan.js
@@ -30,13 +30,13 @@ define(['backbone',
 
             if (options && options.SCANPARAMETERSMODELS) {
                 this.models = options.SCANPARAMETERSMODELS
-                this.listenTo(this.models, 'sync reset change add remove', this.syncModels)
+                this.listenTo(this.models, 'sync reset add remove', this.syncModels)
                 this.syncModels()
             }
 
             if (options && options.DETECTORS) {
                 this.detectors = options.DETECTORS   
-                this.listenTo(this.detectors, 'sync reset change add remove', this.syncDetectors)
+                this.listenTo(this.detectors, 'sync reset add remove', this.syncDetectors)
                 this.syncDetectors()
             }
 
@@ -45,12 +45,14 @@ define(['backbone',
         },
 
         syncModels: function() {
+            if (!this.get('DIFFRACTIONPLANID')) return
             var mods = this.models.where({ DATACOLLECTIONPLANID: this.get('DIFFRACTIONPLANID')})
-            // console.log('syncing models', this.get('DIFFRACTIONPLANID'), mods)
+            console.log('syncing models', this.get('DIFFRACTIONPLANID'), mods)
             this.get('SCANPARAMETERSMODELS').reset(mods)
         },
 
         syncDetectors: function() {
+            if (!this.get('DIFFRACTIONPLANID')) return
             var dets = this.detectors.where({ DATACOLLECTIONPLANID: this.get('DIFFRACTIONPLANID')})
             // console.log('syncing dets', this.get('DIFFRACTIONPLANID'), dets)
             this.get('DETECTORS').reset(dets)
