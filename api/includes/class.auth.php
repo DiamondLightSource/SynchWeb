6ec257d569edcdb99a8d223d00bf60d0ef46aa50
stufisher <stu@q3f.org>
2018-02-13 17:10:36 +0100

    reorder user checks

--- api/includes/class.auth.php
+++ api/includes/class.auth.php
@@ -45,12 +45,10 @@
 				$auth_handler = new $class();
 				$user = $auth_handler->check();
 
-				$userc = $this->db->pq("SELECT personid FROM person WHERE login=:1", array($user));
-				if (!sizeof($userc)) {
-					$this->_error(400, 'No previous session');
-				}
+				if (!$user) $this->_error(400, 'No previous session');
 
-				if ($user) $this->_output(200, $this->generate_jwt($user));
+				$userc = $this->db->pq("SELECT personid FROM person WHERE login=:1", array($user));
+				if (sizeof($userc)) $this->_output(200, $this->generate_jwt($user));
 				else $this->_error(400, 'No previous session');
 			}
 		}
