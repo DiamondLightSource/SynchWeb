712523f3a60335649f7225aa8c8b958e72685c1e
Neil A Smith <neil.smith@diamond.ac.uk>
2018-02-28 13:11:40 +0000

    Updated LDAP authentication code and example settings in config_sample

--- api/includes/class.auth-ldap.php
+++ api/includes/class.auth-ldap.php
@@ -10,16 +10,22 @@ class LDAPAuthentication extends AuthenticationBase implements Authentication {
 
     function authenticate($login, $password) {
         global $ldap_server;
+        global $ldap_search;
 
         $conn = ldap_connect($ldap_server);
 
         if ($conn) {
+            // Tested against LDAP version 3 (could add support for older versions here)
+            ldap_set_option($conn, LDAP_OPT_PROTOCOL_VERSION, 3);
+            
             try {
-                return ldap_bind($conn, $login, $password);
+                // testing with openldap indicates this call needs to use a correct
+                // DN syntax: "uid=<login>,ou=people,dc=example,dc=com"
+                return ldap_bind($conn, "uid=".$login.",".$ldap_search, $password);
 
-            // Coulnt bind
+            // Couldn't bind
             } catch (Exeption $e) {
-                return false
+                return false;
             }
         }
     }
