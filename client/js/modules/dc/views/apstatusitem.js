4e8b81b23aebb2f0321aac6d6f6a61a833acc66d
stufisher <stu@q3f.org>
2018-11-15 14:55:04 +0100

    apstatus knows about xrc

--- client/js/modules/dc/views/apstatusitem.js
+++ client/js/modules/dc/views/apstatusitem.js
@@ -6,7 +6,6 @@ define(['marionette', 'jquery'], function(Marionette, $) {
   
         ui: {
             holder: '.holder h1 span',
-            flux: '.flux',
         },
     
         initialize: function(options) {
@@ -22,6 +21,14 @@ define(['marionette', 'jquery'], function(Marionette, $) {
                 this.delegateEvents()
                 if (this.model) this.render()
             }
+            this.trigger('model:change')
+        },
+
+        getStatus: function(options) {
+            var res = this.model.get('STATES')
+            if (options.type in res) {
+                return res[options.type]
+            }
         },
         
         onRender: function() {
@@ -34,20 +41,23 @@ define(['marionette', 'jquery'], function(Marionette, $) {
                     '<i class="fa icon grey fa-cog fa-spin alt="Running"></i>',
                     '<i class="fa icon green fa-check alt="Completed"></i>',
                     '<i class="fa icon red fa-times alt="Failed"></i>']
-        
-           if (this.getOption('SCREEN')) {
-               this.ui.holder.html('Mosflm: ' + val[res['Mosflm']] + ' EDNA: ' + val[res['EDNA']] + ' Xia2: ' + val[res['Xia2/strat']])
+
+            if (this.getOption('XRC')) {
+                this.ui.holder.html('Xray Centring: ' + val[res['XrayCentring']])
+
+            } else if (this.getOption('SCREEN')) {
+                this.ui.holder.html('Mosflm: ' + val[res['Mosflm']] + ' EDNA: ' + val[res['EDNA']] + ' Xia2: ' + val[res['Xia2/strat']])
                
-           } else {
-               this.ui.holder.eq(0).html('Fast DP: ' + val[res['Fast DP']] +
-                         ' Xia2: ' + val[res['Xia2/3d']] + ' ' +val[res['Xia2/3dii']] + ' ' +val[res['DIALS']] +
-                         ' MultiXia2: ' + val[res['MultiXia2/XDS']] + ' ' +val[res['MultiXia2/DIALS']] +
-                         ' autoPROC: ' + val[res['autoPROC']])
-               this.ui.holder.eq(1).html( 'Fast EP: ' + val[res['Fast EP']] +
-                                         ' Big EP: ' + val[res['Big EP/XDS']] + ' '+ val[res['Big EP/DIALS']] +
-                                         ' Dimple: ' + val[res['Dimple']] +
-                                         ' MrBUMP: ' + val[res['MrBUMP']])
-           }
+            } else {
+                this.ui.holder.eq(0).html('Fast DP: ' + val[res['Fast DP']] +
+                    ' Xia2: ' + val[res['Xia2/3d']] + ' ' +val[res['Xia2/3dii']] + ' ' +val[res['DIALS']] +
+                    ' MultiXia2: ' + val[res['MultiXia2/XDS']] + ' ' +val[res['MultiXia2/DIALS']] +
+                    ' autoPROC: ' + val[res['autoPROC']])
+                this.ui.holder.eq(1).html( 'Fast EP: ' + val[res['Fast EP']] +
+                    ' Big EP: ' + val[res['Big EP/XDS']] + ' '+ val[res['Big EP/DIALS']] +
+                    ' Dimple: ' + val[res['Dimple']] +
+                    ' MrBUMP: ' + val[res['MrBUMP']])
+            }
         }
     })
        
