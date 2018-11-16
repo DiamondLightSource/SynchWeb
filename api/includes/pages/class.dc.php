49bd5b3fbad1aaaf16c00b951b5bfa317c675e7e
stufisher <stu@q3f.org>
2018-11-16 15:21:25 +0100

    return status and message with autoproc

--- api/includes/pages/class.dc.php
+++ api/includes/pages/class.dc.php
@@ -1061,7 +1061,7 @@
         # Auto processing for a data collection
         function _dc_auto_processing($id) {
             $rows = $this->db->pq('SELECT apss.cchalf, apss.ccanomalous, apss.anomalous, dc.xbeam, dc.ybeam, api.refinedxbeam, api.refinedybeam, app.autoprocprogramid,app.processingprograms as type, apss.ntotalobservations as ntobs, apss.ntotaluniqueobservations as nuobs, apss.resolutionlimitlow as rlow, apss.resolutionlimithigh as rhigh, apss.scalingstatisticstype as shell, apss.rmeasalliplusiminus as rmeas, apss.rmerge, apss.completeness, apss.anomalouscompleteness as anomcompleteness, apss.anomalousmultiplicity as anommultiplicity, apss.multiplicity, apss.meanioversigi as isigi, ap.spacegroup as sg, ap.refinedcell_a as cell_a, ap.refinedcell_b as cell_b, ap.refinedcell_c as cell_c, ap.refinedcell_alpha as cell_al, ap.refinedcell_beta as cell_be, ap.refinedcell_gamma as cell_ga, 
-                    (SELECT COUNT(api1.autoprocintegrationid) FROM autoprocintegration api1 WHERE api1.autoprocprogramid =  app.autoprocprogramid) as nswps
+                    (SELECT COUNT(api1.autoprocintegrationid) FROM autoprocintegration api1 WHERE api1.autoprocprogramid =  app.autoprocprogramid) as nswps, app.processingstatus, app.processingmessage
                 FROM autoprocintegration api 
                 LEFT OUTER JOIN autoprocscaling_has_int aph ON api.autoprocintegrationid = aph.autoprocintegrationid 
                 LEFT OUTER JOIN autoprocscaling aps ON aph.autoprocscalingid = aps.autoprocscalingid 
@@ -1137,6 +1137,8 @@
                 }
                 
                 $output[$r['AUTOPROCPROGRAMID']]['AID'] = $r['AUTOPROCPROGRAMID'];
+                $output[$r['AUTOPROCPROGRAMID']]['PROCESSINGSTATUS'] = $r['PROCESSINGSTATUS'];
+                $output[$r['AUTOPROCPROGRAMID']]['PROCESSINGMESSAGE'] = $r['PROCESSINGMESSAGE'];
                 $output[$r['AUTOPROCPROGRAMID']]['SHELLS'][$r['SHELL']] = $shell;
                 $output[$r['AUTOPROCPROGRAMID']]['MESSAGES'] = array_key_exists($r['AUTOPROCPROGRAMID'], $messages) ? $messages[$r['AUTOPROCPROGRAMID']] : array();
             }
