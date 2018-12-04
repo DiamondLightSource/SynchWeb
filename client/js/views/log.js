027e33a737de7095a686dde6e029af5cd122cef5
stufisher <stu@q3f.org>
2018-12-04 16:33:03 +0100

    deal with binary mime types in iframe (ajax -> xmlhttpreq)

--- client/js/views/log.js
+++ client/js/views/log.js
@@ -13,57 +13,80 @@ define(['marionette', 'views/dialog', 'utils'], function(Marionette, DialogView,
         
         load: function() {
             var self = this
-            Backbone.ajax({
-                url: this.url,
-                success: function(content, status, xhr) {
-                    var ct = xhr.getResponseHeader('content-type')
-                    if (ct.indexOf('text/plain') > -1) content = '<pre>'+content+'</pre>'
 
-                    // inject sign handler
-                    var sh = '<script src="/client/js/vendor/jquery/jquery-1.9.1.min.js"></script>\n\
-                    <script type="text/javascript">\n\
-                        $(document).ready(function() {\n\
-                            var app = { apiurl: "'+app.apiurl+'", prop: "'+app.prop+'"}\n\
-                            app.token = sessionStorage.getItem(\'token\')\n\
-                            var root = "'+self.url.replace(/\?token=.*/, '')+'"\n\
-                            var ajax = function(options) {\n\
-                                options.data.prop = app.prop\n\
-                                if (app.token) {\n\
-                                    options.beforeSend = function(request){\n\
-                                        request.setRequestHeader(\'Authorization\', \'Bearer \' + app.token);\n\
-                                    }\n\
+            var xhr = new XMLHttpRequest()
+            xhr.open('GET', this.url, true)
+            xhr.responseType = 'arraybuffer'
+
+            xhr.onload = function(e) {
+                if (xhr.status == 0 || xhr.status != 200) {
+                    return
+                }
+
+                var h = xhr.getAllResponseHeaders()
+                var m = h.match(/^Content-Type\:\s*(.*?)$/mi)
+                var mimeType = m[1] || 'image/png'
+
+                var blob = new Blob([this.response], { type: mimeType })
+
+                // inject sign handler
+                var sh = '<script src="/client/js/vendor/jquery/jquery-1.9.1.min.js"></script>\n\
+                <script type="text/javascript">\n\
+                    $(document).ready(function() {\n\
+                        var app = { apiurl: "'+app.apiurl+'", prop: "'+app.prop+'"}\n\
+                        app.token = sessionStorage.getItem(\'token\')\n\
+                        var root = "'+self.url.replace(/\?token=.*/, '')+'"\n\
+                        var ajax = function(options) {\n\
+                            options.data.prop = app.prop\n\
+                            if (app.token) {\n\
+                                options.beforeSend = function(request){\n\
+                                    request.setRequestHeader(\'Authorization\', \'Bearer \' + app.token);\n\
                                 }\n\
-                                return $.ajax.call(this, options)\n\
                             }\n\
-                            var Backbone = { ajax: ajax }\n\
-                            var sign = '+utils.sign.toString()+'\n\
-                            $("a").click(function(e) {\n\
-                                var is_relative_to_page = function(href) {\n\
-                                    return href.match(/^\\/|(http:|https:|ftp:|mailto:|javascript:)/) === null;\n\
+                            return $.ajax.call(this, options)\n\
+                        }\n\
+                        var Backbone = { ajax: ajax }\n\
+                        var sign = '+utils.sign.toString()+'\n\
+                        $("a").click(function(e) {\n\
+                            var is_relative_to_page = function(href) {\n\
+                                return href.match(/^\\/|(http:|https:|ftp:|mailto:|javascript:)/) === null;\n\
+                            }\n\
+                            var is_routable = function(href) {\n\
+                                return href.indexOf("#") === -1 && is_relative_to_page(href);\n\
+                            }\n\
+                            var href = $(this).attr("href")\n\
+                            if (!is_routable(href)) return true\n\
+                            e.preventDefault()\n\
+                            var url = root+\'/\'+href\n\
+                            \sign({\n\
+                                url: url,\n\
+                                callback: function(resp) {\n\
+                                    window.location = url+\'?token=\'+resp.token\n\
                                 }\n\
-                                var is_routable = function(href) {\n\
-                                    return href.indexOf("#") === -1 && is_relative_to_page(href);\n\
-                                }\n\
-                                var href = $(this).attr("href")\n\
-                                if (!is_routable(href)) return true\n\
-                                e.preventDefault()\n\
-                                var url = root+\'/\'+href\n\
-                                \sign({\n\
-                                    url: url,\n\
-                                    callback: function(resp) {\n\
-                                        window.location = url+\'?token=\'+resp.token\n\
-                                    }\n\
-                                })\n\
                             })\n\
-                        });\n\
-                    </script>'
+                        })\n\
+                    });\n\
+                </script>'
 
+                if (mimeType == 'application/pdf' || mimeType == 'image/png') {
+                    var url = URL.createObjectURL(new Blob([this.response], { type: mimeType }))
+                    self.iframe[0].src = url
+
+                } else {
                     var doc = self.iframe[0].contentWindow.document
+                    var dec = new TextDecoder('utf-8')
+                    var text = dec.decode(this.response)
+
+                    if (mimeType.indexOf('text/plain') > -1) text = '<pre>'+text+'</pre>'
+
                     doc.open()
-                    doc.write(sh+content)
+                    doc.write(sh+text)
                     doc.close()
                 }
-            })
+
+            }
+
+            xhr.send()
         },
         
         onRender: function() {
