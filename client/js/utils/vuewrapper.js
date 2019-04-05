4010f8dc32d20b4d0d9b78c36a8f326eb7f94026
Neil Smith <neil.smith@diamond.ac.uk>
2019-04-05 11:52:00 +0100

    Cleaner approach to allow vue to access models and collections on view creation

--- client/js/utils/vuewrapper.js
+++ client/js/utils/vuewrapper.js
@@ -1,5 +1,5 @@
 // This module wraps a Vue application into a Marionette view
-define(['marionette'], function(Marionette) {
+define(['marionette', 'vue'], function(Marionette, Vue) {
     return Marionette.ItemView.extend({
         vueView: null,
         // class or id?
@@ -10,11 +10,15 @@ define(['marionette'], function(Marionette) {
         },
         // Backbone Render method - where we attach the Vue app to the page
         onRender: function() {
+            // This allows the vue component to access any constructor arguments
+            // They can then be accessible in the created lifecycle hook
+            // Works with the assumption we only have a single vue instance per page
+            let self = this
+            Vue.prototype.$getOption = function(option) {
+                return self.getOption(option)
+            }
+
             this.vue = new this.vueView({el: this.ui.container[0]})
-            // Set any passed model, collection to the vue instance
-            // Declare this within the vue component's data object as m_model, m_collection
-            this.vue.m_model = this.model
-            this.vue.m_collection = this.collection
         },
 
         // Backbone View Lifecycle hook
