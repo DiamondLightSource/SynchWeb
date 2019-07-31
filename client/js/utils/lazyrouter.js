c24086047a9728106c0099be177535c9045b2819
Neil A Smith <neil.smith@diamond.ac.uk>
2019-07-31 18:22:43 +0100

    Update to lazy router to work with webpack dynamic imports

--- client/js/utils/lazyrouter.js
+++ client/js/utils/lazyrouter.js
@@ -10,20 +10,21 @@ define(['marionette'], function(Marionette) {
     return LazyRouter = Marionette.AppRouter.extend({
         /** @property {string} Controller to lazy load when required */
         // relative/path/to/controller
-        rjsController: null,
+        // rjsController: null, // Not using in favour of dynamic import
         
         /** @property {string} Events to trigger a lazy load */
         loadEvents: [],
-        
+
+        loadModule: null, // Callback function that loads module
         
         initialize: function(options) {
             LazyRouter.__super__.initialize.apply(this, options)
             
             this.lazyloaded = false
             
-            if (!this.getOption('rjsController')) {
-                //throw new Error('No controller specified')
-            }
+            // if (!this.getOption('rjsController')) {
+            //     //throw new Error('No controller specified')
+            // }
             
             _.each(this.getOption('loadEvents'), function(e) {
                 app.on(e, this.lazyLoad.bind(this,e))
@@ -33,22 +34,31 @@ define(['marionette'], function(Marionette) {
         lazyLoad: function(e) {
             var self = this
             var args = arguments
-            if (!this.lazyloaded) {
-                require([this.getOption('rjsController')], function(c) {
+
+            if (!this.lazyloaded && this.loadModule) {
+                this.loadModule(function(controller) {
                     self.lazyloaded = true
                     console.log(e, args)
                     app.trigger.apply(app, args)
                 })
+            } else {
+                console.log("LazyLoader::Error trying to lazy load module ")
             }
         },
         
         _addAppRoute: function(controller, route, methodName) {
             var proxy = function() {
+                var self = this
                 var args = arguments
-                require([this.getOption('rjsController')], function(c) {
-                    c[methodName].apply(c, args)
-                    this.lazyloaded = true
-                })
+
+                if (this.loadModule) {
+                    this.loadModule(function(controller) {
+                        controller[methodName].apply(controller, args)
+                        self.lazyloaded = true
+                    })
+                } else {
+                    console.log("Error trying to lazy load module ")
+                }
             }
             
             this.route(route, methodName, proxy);
