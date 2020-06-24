ef0680bd3a0f97eee9e4630e3601adb8e6231e2c
Stu Fisher <contact@roygbiv.org.uk>
2020-06-24 17:28:40 +0200

    add timezone to options

--- api/index.php
+++ api/index.php
@@ -50,7 +50,7 @@ $app->configureMode('development', function () use ($app) {
 $app->get('/options', function () use ($app) {
     global $motd, $authentication_type, $cas_url, $cas_sso, $package_description, $facility_courier_countries, $facility_courier_countries_nde, $dhl_enable, $dhl_link, $scale_grid, $preset_proposal, $ap_statuses;
     $app->contentType('application/json');
-    $app->response()->body(json_encode(array('motd' => $motd, 'authentication_type' => $authentication_type, 'cas_url' => $cas_url, 'cas_sso' => $cas_sso, 'package_description' => $package_description, 'facility_courier_countries' => $facility_courier_countries, 'facility_courier_countries_nde' => $facility_courier_countries_nde, 'dhl_enable' => $dhl_enable, 'dhl_link' => $dhl_link, 'scale_grid' => $scale_grid, 'preset_proposal' => $preset_proposal, 'ap_statuses' => $ap_statuses['types'])));
+    $app->response()->body(json_encode(array('motd' => $motd, 'authentication_type' => $authentication_type, 'cas_url' => $cas_url, 'cas_sso' => $cas_sso, 'package_description' => $package_description, 'facility_courier_countries' => $facility_courier_countries, 'facility_courier_countries_nde' => $facility_courier_countries_nde, 'dhl_enable' => $dhl_enable, 'dhl_link' => $dhl_link, 'scale_grid' => $scale_grid, 'preset_proposal' => $preset_proposal, 'ap_statuses' => $ap_statuses['types'], 'timezone' => $timezone)));
     // $app->response()->body(json_encode($options->ui()));
 });
 
