4e6176ccaea18568475688043f5ba8f591285d70
Neil Smith <neil.smith@diamond.ac.uk>
2020-09-25 12:44:06 +0100

    Show yellow rated samples when adding containers
    Might need to undo this change because colours may confuse with form validation

--- client/src/js/utils/kvcollection.js
+++ client/src/js/utils/kvcollection.js
@@ -1,14 +1,17 @@
 define(['backbone'], function(Backbone) {
     
     return {
+	    // Updated options so you can pass a callback function.
+        // The callback will receive the model and should return the class to add for that entry
         opts: function(options) {
             return (options && options.empty ? '<option value=""> - </option>' : '') +
-                this.map(function(m) { 
+                this.map(function(m) {
                     var cl= ''
-                    if (options && options.addClass && options.classProperty) {
+                    if (options && options.callback instanceof Function) cl = options.callback(m)
+                    else if (options && options.addClass && options.classProperty) {
                         if (m.get(options.classProperty) == options.classPropertyValue) cl = options.addClass
                     }
-                    return '<option class="'+cl+'" value="'+m.escape(this.valueAttribute)+'">'+m.escape(this.keyAttribute)+'</option>' 
+                    return '<option class="'+cl+'" value="'+m.escape(this.valueAttribute)+'">'+m.escape(this.keyAttribute)+'</option>'
                 }, this).join('\n')
         },
 
