f6e3cb08b9d63669b5ec7e85410b23b369ea1045
Stu Fisher <contact@roygbiv.org.uk>
2020-09-29 14:21:10 +0200

    tab -> space

--- client/src/js/modules/types/gen/samples/views/componentlist.js
+++ client/src/js/modules/types/gen/samples/views/componentlist.js
@@ -1,20 +1,20 @@
 define(['modules/samples/views/proteinlist', 'utils/table'], function(ProteinList, table) {
-	
-	return ProteinList.extend({
-		title: 'Component',
+    
+    return ProteinList.extend({
+        title: 'Component',
 
-		columns: [
-	        { name: 'NAME', label: 'Name', cell: 'string', editable: false },
-	        { name: 'ACRONYM', label: 'Acronym', cell: 'string', editable: false },
-	        { name: 'MOLECULARMASS', label: 'Mass', cell: 'string', editable: false },
-	        { name: 'COMPONENTTYPE', label: 'Type', cell: 'string', editable: false },
-	        { name: 'SCOUNT', label: 'Samples', cell: 'string', editable: false },
-			{ name: 'DCOUNT', label: 'Data Collections', cell: 'string', editable: false },
-			{ name: 'SAFETYLEVEL', label: 'Risk Rating', cell: 'string', editable: false },
-	        { name: ' ', cell: table.ProjectCell, itemname: 'ACRONYM', itemid: 'PROTEINID', itemtype:'protein', editable: false },
-	    ],
+        columns: [
+            { name: 'NAME', label: 'Name', cell: 'string', editable: false },
+            { name: 'ACRONYM', label: 'Acronym', cell: 'string', editable: false },
+            { name: 'MOLECULARMASS', label: 'Mass', cell: 'string', editable: false },
+            { name: 'COMPONENTTYPE', label: 'Type', cell: 'string', editable: false },
+            { name: 'SCOUNT', label: 'Samples', cell: 'string', editable: false },
+            { name: 'DCOUNT', label: 'Data Collections', cell: 'string', editable: false },
+            { name: 'SAFETYLEVEL', label: 'Risk Rating', cell: 'string', editable: false },
+            { name: ' ', cell: table.ProjectCell, itemname: 'ACRONYM', itemid: 'PROTEINID', itemtype:'protein', editable: false },
+        ],
 
-	    hiddenColumns: [2,3],
-	})
+        hiddenColumns: [2,3],
+    })
 
 })
