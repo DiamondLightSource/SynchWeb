a443df42547e8d8141ed74bb924c66c7ec6262e9
Neil A Smith <neil.smith@diamond.ac.uk>
2020-10-09 11:31:50 +0100

    Fix attachment links for data collection groups

--- client/src/js/modules/dc/views/attachments.js
+++ client/src/js/modules/dc/views/attachments.js
@@ -17,11 +17,15 @@ define(['marionette',
         },
 
         render: function() {
+            // This was using an 'id' passed into the column as the dcid (getOption('id')).
+            // However, this is not present when loading attachments from a data collection group
+            // Use the value from the queried collection instead
+            var dcid = this.model.escape('DATACOLLECTIONID')
 
-            this.$el.append('<a href="'+app.apiurl+'/download/attachment/id/'+this.column.escape('id')+'/aid/'+this.model.escape('DATACOLLECTIONFILEATTACHMENTID')+'" class="button dl"><i class="fa fa-download"></i> Download</a>')
+            this.$el.append('<a href="'+app.apiurl+'/download/attachment/id/'+dcid+'/aid/'+this.model.escape('DATACOLLECTIONFILEATTACHMENTID')+'" class="button dl"><i class="fa fa-download"></i> Download</a>')
 
             if (this.model.get('FILETYPE') == 'recip') {
-                this.$el.append('<a href="/dc/rsv/id/'+this.column.escape('id')+'" class="button rsv"><i class="fa fa-search"></i> Reciprocal Space Viewer</a>')
+                this.$el.append('<a href="/dc/rsv/id/'+dcid+'" class="button rsv"><i class="fa fa-search"></i> Reciprocal Space Viewer</a>')
             }
 
             return this
@@ -45,7 +49,7 @@ define(['marionette',
             var columns = [
                 { name: 'FILEFULLPATH', label: 'File', cell: 'string', editable: false },
                 { name: 'FILETYPE', label: 'Type', cell: 'string', editable: false },
-                { label: '', cell: OptionsCell, editable: false, id: this.getOption('id') },
+                { label: '', cell: OptionsCell, editable: false },
             ]
                         
             this.table = new TableView({ 
