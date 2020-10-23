f11bb5eeefd76366c4c06b9166c5d0a2879659b1
Stu Fisher <contact@roygbiv.org.uk>
2020-10-23 09:46:48 +0200

    fix ap summary for failed aps, be more informative about ap statuses, set bestap if only a single ap

--- client/src/js/modules/dc/views/summary.js
+++ client/src/js/modules/dc/views/summary.js
@@ -1,22 +1,16 @@
-define(['marionette', 
-    'backgrid', 
-    'views/table', 
-    'models/datacollection', 
-    'modules/dc/collections/apstatuses', 
-    'modules/dc/collections/autointegrations', 
+define(['marionette',
+    'backgrid',
+    'views/table',
+    'models/datacollection',
+    'modules/dc/collections/autointegrations',
     'utils',
+    'utils/table',
     'templates/dc/summary.html'], 
-    function(Marionette, Backgrid, TableView, DataCollection, APStatuses, AutoIntegrations, 
+    function(Marionette, Backgrid, TableView, DataCollection, AutoIntegrations, 
         utils,
+        table,
         template) {
 
-    
-    var APDataCollection = DataCollection.extend({
-        defaults: {
-            APLOADED: false,
-        },
-    })
-    
     
     var APCell = Backgrid.Cell.extend({
         initialize: function(options) {
@@ -24,17 +18,23 @@ define(['marionette',
             this.listenTo(this.model, 'change reset', this.render, this)
             this.template = options.column.get('template')
         },
-        
+
         render: function() {
             this.$el.empty();
             
-            if (this.model.get('APLOADED') == true && this.model.get('BESTAP')) {
+            if (this.model.get('BESTAP')) {
                 var b = this.model.get('BESTAP')
                 var t = _.template(_.result(this, 'template'))
                 this.$el.html(t(b.toJSON()))
-                
+            
             } else if (this.model.get('APLOADED') == true) {
-                this.$el.html('')
+                if (this.column.get('first')) {
+                    if (this.model.get('AUTOINTEGRATIONS').length == 0) this.$el.text('No successful auto processing')
+                    else this.$el.text('No best auto processing determined')
+
+                } else {
+                    this.$el.text('')
+                }
                 
             } else {
                 this.$el.html('<i class="fa fa-spin fa-spinner"></i>')
@@ -46,6 +46,22 @@ define(['marionette',
         }
     })
     
+
+    var APLenCell = Backgrid.Cell.extend({
+        initialize: function(options) {
+            APLenCell.__super__.initialize.call(this,options)
+            this.listenTo(this.model, 'change reset', this.render, this)
+        },
+
+        render() {
+            if (this.model.get('APLOADED')) {
+                this.$el.text(this.model.get('AUTOINTEGRATIONS').length)
+            } else this.$el.text('')
+
+            return this
+        }
+    })
+
     return Marionette.LayoutView.extend({
         className: 'content',
         template: template,
@@ -71,36 +87,31 @@ define(['marionette',
             sg: 'input[name="sg"]',
         },
         
-        initialize: function(options) {
-            this.statuses = new APStatuses()
-        },
-        
         updateStatus: function() {
             console.log('updating status')
             var self = this
             var deferreds = []
-            this.statuses.fetch({ type: 'POST', data: { ids: this.collection.pluck('ID') } }).done(function() {
-                self.statuses.each(function(s) {
-                    var ai = new AutoIntegrations(null, { id: s.get('ID') })
-                    var d = ai.fetch().then(function() {
-                        var dc = self.collection.findWhere({ ID: s.get('ID') })
-                        dc.set({ AUTOINTEGRATIONS: ai }, { silent: true })
-                    })
-                    deferreds.push(d)
+            this.collection.each(function(dc) {
+                var ai = new AutoIntegrations(null, { id: dc.get('ID') })
+                var d = ai.fetch().then(function() {
+                    var filtered = ai.where({ PROCESSINGSTATUS: "1"})
+                    dc.set({ AUTOINTEGRATIONS: new AutoIntegrations(filtered, { id: dc.get('ID') }) }, { silent: true })
                 })
+                deferreds.push(d)
+            })
                 
-                $.when.apply($, deferreds).then(function() {
-                    self.collection.each(function(m) {
-                        m.set({ APLOADED: true }, { silent: m.get('AUTOINTEGRATIONS').length ? true: false })
-                    })
-                    self.scoreAPS()
+            $.when.apply($, deferreds).then(function() {
+                self.collection.each(function(m) {
+                    m.set({ APLOADED: true }, { silent: m.get('AUTOINTEGRATIONS').length ? true: false })
                 })
+                self.scoreAPS()
             })
         },
 
         
         onRender: function() {
             var columns = [
+                { label: '', cell: table.TemplateCell, editable: false, template: '<a href="/dc/id/<%-ID%>" class="button button-notext"><i class="fa fa-search"></i> <span>View Data Collection</span></a>' },
                 { name: 'FILETEMPLATETRIM', label: 'Prefix', cell: 'string', editable: false },
                 { name: 'SAMPLE', label: 'Sample', cell: 'string', editable: false },
                 { name: 'ST', label: 'Date', cell: 'string', editable: false },
@@ -108,7 +119,8 @@ define(['marionette',
                 { name: 'AXISRANGE', label: 'Osc', cell: 'string', editable: false },
                 { name: 'EXPOSURETIME', label: 'Exposure', cell: 'string', editable: false },
                 { name: 'TRANSMISSION', label: 'Transmission', cell: 'string', editable: false },
-                { label: 'Spacegroup', cell: APCell, template: '<%-SG%> <br /><%-TYPE%>', editable: false },
+                { label: '#AP', cell: APLenCell, editable: false },
+                { label: 'Spacegroup', cell: APCell, template: '<%-SG%> <br /><%-TYPE%>', editable: false, first: true },
                 { label: 'Unit Cell', cell: APCell, template: '<%-CELL.CELL_A%> (<%-CELL.CELL_AL%>)<br /><%-CELL.CELL_B%> (<%-CELL.CELL_BE%>)<br /><%-CELL.CELL_C%> (<%-CELL.CELL_GA%>)', editable: false },
                 { label: 'Resolution', cell: APCell, template: '<%-SHELLS.overall.RLOW%> - <%-SHELLS.overall.RHIGH%><br /><%-SHELLS.innerShell.RLOW%> - <%-SHELLS.innerShell.RHIGH%><br /><%-SHELLS.outerShell.RLOW%> - <%-SHELLS.outerShell.RHIGH%>', editable: false },
                 { label: 'Rmeas', cell: APCell, template: '<span class="<%-CLASS.RMEAS.overall%>"><%-SHELLS.overall.RMEAS%></span><br /><span class="<%-CLASS.RMEAS.innerShell%>"><%-SHELLS.innerShell.RMEAS%></span><br /><span class="<%-CLASS.RMEAS.outerShell%>"><%-SHELLS.outerShell.RMEAS%></span>', editable: false },
@@ -123,7 +135,7 @@ define(['marionette',
                 tableClass: '',
                 filter: 's',
                 loading: true,
-                backgrid: { emptyText: 'No data collections found', }
+                backgrid: { emptyText: 'No data collections found' }
             
             }))
             
@@ -159,8 +171,6 @@ define(['marionette',
                 if (dc.get('APLOADED')) {
                     var aps = dc.get('AUTOINTEGRATIONS')
                     aps.each(function(d) {
-                        if (d.get('PROCESSINGSTATUS') != 1) return
-
                         _.each(sweights, function(w, sh) {
                             var s = d.get('SHELLS')[sh]
                             if (parseFloat(s.RHIGH) > max.res[sh]) max.res[sh] = parseFloat(s.RHIGH)
@@ -203,6 +213,7 @@ define(['marionette',
 
                     //console.log('setting best', best)
                     if (best) dc.set('BESTAP', best)
+                    else if (aps.length == 1) dc.set('BESTAP', aps.at(0))
                 }
             }, this)
                                 
