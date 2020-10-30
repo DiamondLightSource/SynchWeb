eda5f168278d9ed2aa485444b35caaea7c169866
Victor Nwaiwu <vonvikky@gmail.com>
2020-10-30 00:04:25 +0000

    fix: add comment to explain why we added is_numeric check to values

--- api/src/Page/DC.php
+++ api/src/Page/DC.php
@@ -1204,6 +1204,8 @@ class DC extends Page
                         if ($k == 'CCANOMALOUS') $v = number_format($v, 1);
 
                         $beam = array('XBEAM', 'YBEAM', 'REFINEDXBEAM', 'REFINEDYBEAM');
+                        
+                        // We need to discriminate between NULL values from the database and when the value is a zero
                         if (in_array($k, $beam) && is_numeric($v)) $v = number_format($v, 2);
                         
                         if ($k == 'AUTOPROCPROGRAMID' || $k == 'SHELL' || $k == 'PROCESSINGJOBID' || $k == 'IMAGESWEEPCOUNT') {
