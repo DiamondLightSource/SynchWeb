97f904b2f1f1d85f85af62b8517b099193a36d31
Victor Nwaiwu <vonvikky@gmail.com>
2020-11-13 14:22:16 +0000

    fix: import only required module for date-fns

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -106,7 +106,7 @@ module.exports = (env) => ({
       // Vue packages from npm (vee-validate requires promise polyfill - also npm)
       vue: 'vue/dist/vue.min',
       veevalidate: 'vee-validate/dist/vee-validate.min',
-      dateFnsTz: 'date-fns-tz'
+      zonedTimeToUtc: 'date-fns-tz/zonedTimeToUtc'
     },
     modules: [
       path.resolve(__dirname, 'src/js'),
