4e9278162c1d5c468fb7d38e4887cbb657d1ab90
Neil A Smith <neil.smith@diamond.ac.uk>
2020-11-27 17:07:40 +0000

    Adding purgecss to remove unused styles

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,5 +1,16 @@
 // postcss.config.js
 const path = require('path');
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+  // Specify the paths to all of the template files in your project
+  content: [
+    './src/index.php',
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.js', /* Lots of classes are used within inline templates... */
+  ],
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+})
 
 module.exports = ({env}) => (
   {
@@ -16,6 +27,7 @@ module.exports = ({env}) => (
       require('postcss-color-function'),
       require('tailwindcss'),
       require('autoprefixer'),
+      ...process.env.NODE_ENV === 'production' ? [purgecss]: [],
     ]
   }
 )
