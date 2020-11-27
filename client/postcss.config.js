fd199b7f747afc2baadb3649a5b1994a90849afa
Neil A Smith <neil.smith@diamond.ac.uk>
2020-11-27 12:58:47 +0000

    Add tailwindcss

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -14,6 +14,7 @@ module.exports = ({env}) => (
       require('postcss-extend'),
       require('postcss-strip-inline-comments'),
       require('postcss-color-function'),
+      require('tailwindcss'),
       require('autoprefixer'),
     ]
   }
