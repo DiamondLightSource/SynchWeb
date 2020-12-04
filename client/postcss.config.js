80bdefa29e1d25628fc01c5ff7f684a36092d426
Neil A Smith <neil.smith@diamond.ac.uk>
2020-12-04 13:58:57 +0000

    Switching to postcss-utilities for clearfix (copes with edge cases)

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -20,11 +20,13 @@ module.exports = ({env}) => (
       require('postcss-mixins')({
         mixinsDir: path.join(__dirname, 'src', 'css', 'mixins')
       }),
+      // Using utilities for clearfix - ie8 style is closer to original one used
+      require('postcss-utilities')({ ie8: true}),
       require('postcss-simple-vars'),
-      require('postcss-nested'),
-      require('postcss-extend'),
       require('postcss-strip-inline-comments'),
       require('postcss-color-function'),
+      require('postcss-nested'),
+      require('postcss-extend-rule'),
       require('tailwindcss'),
       require('autoprefixer'),
       ...process.env.NODE_ENV === 'production' ? [purgecss]: [],
