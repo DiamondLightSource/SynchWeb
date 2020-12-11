bec739f418a3db7fb5e804a6d4fb38aa24df11c2
Neil A Smith <neil.smith@diamond.ac.uk>
2020-12-11 18:58:46 +0000

    Fix statistics routing

--- client/src/js/modules/blstats/routes.js
+++ client/src/js/modules/blstats/routes.js
@@ -4,33 +4,25 @@
 // Models and collections can be specified as props
 // If a model or collection is passed in the data will be prefetched before the component is loaded
 // Props that make use of the route object should use props: route => ({ ...define prop using route.params object})
-import Page from 'app/layouts/page.vue'
-import Debug from 'app/views/debug.vue'
 import MarionetteView from 'app/views/marionette/marionette-wrapper.vue'
 import StatsView from 'modules/blstats/views/stats'
 
-let bc = { title: 'Usage Stats', url: '/ustats' }
+let bc = { title: 'Usage Stats', url: '/statistics' }
 
 const routes = [
   {
-    path: '/statistics',
-    component: Page,
-    children: [
-        {
-            // We need to pass the regex to allow capture of the second optional parameter
-            path: ':type([a-zA-Z0-9]+)?(/)?:subtype?',
-            name: 'statistics',
-            component: MarionetteView,
-            props: route => ({
-                mview: StatsView,
-                breadcrumbs: [bc],
-                options: {
-                    type: route.params.type || '', 
-                    subtype: route.params.subtype || ''
-                }
-            })
+    // We need to pass the regex to allow capture of the second optional parameter
+    path: '/statistics(/)?:type([a-zA-Z0-9]+)?(/)?:subtype?',
+    name: 'statistics',
+    component: MarionetteView,
+    props: route => ({
+        mview: StatsView,
+        breadcrumbs: [bc],
+        options: {
+            type: route.params.type || '',
+            subtype: route.params.subtype || ''
         },
-    ]
+    })
   },
 ]
 
