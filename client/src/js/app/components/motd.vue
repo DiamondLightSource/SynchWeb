66e106ce38f4d7a2dab1bb2b8e03b2de7836fa1f
Neil A Smith <neil.smith@diamond.ac.uk>
2021-03-08 14:18:46 +0000

    Only show motd if there is one

--- client/src/js/app/components/motd.vue
+++ client/src/js/app/components/motd.vue
@@ -1,5 +1,5 @@
 <template>
-    <div v-if="!closed" id="motd-banner" class="tw-hidden md:tw-flex tw-mx-4 tw-my-4 tw-p-4 tw-bg-content-active tw-rounded tw-flex tw-justify-between">
+    <div v-if="!closed && message" id="motd-banner" class="tw-hidden md:tw-flex tw-mx-4 tw-my-4 tw-p-4 tw-bg-content-active tw-rounded tw-flex tw-justify-between">
         <p class="tw-text-md">{{message}}</p>
         <i @click="closed = true" class="fa fa-times"/>
     </div>
