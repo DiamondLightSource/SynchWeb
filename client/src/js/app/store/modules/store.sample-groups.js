1c235d5473eff69483c012f12b93b36e6a262a40
Victor Nwaiwu <vonvikky@gmail.com>
2021-03-20 14:44:13 +0000

    refactor: make changes based on feedback on task

--- client/src/js/app/store/modules/store.sample-groups.js
+++ client/src/js/app/store/modules/store.sample-groups.js
@@ -1,6 +1,6 @@
 export default {
   state: {
-    selectedSampleGroups: [],
+    selectedSampleGroups: {},
     sampleGroups: [],
     sampleGroupsContainers: [],
     selectedSampleGroupName: ''
@@ -15,10 +15,13 @@ export default {
       state.sampleGroups = data
     },
     setSelectedSampleGroups(state, data) {
-      state.selectedSampleGroups = data
+      state.selectedSampleGroups = { ...state.selectedSampleGroups, ...data }
     },
     setSelectedSampleGroupName(state, data) {
       state.selectedSampleGroupName = data
+    },
+    resetSelectedSampleGroups(state) {
+      state.selectedSampleGroups = {}
     }
   },
   actions: {},
