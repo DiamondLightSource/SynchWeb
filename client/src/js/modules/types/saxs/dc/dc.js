a2da0e5814848c7980b0da8474430a093e18bdf7
Stu Fisher <contact@roygbiv.org.uk>
2021-03-22 15:36:50 +0100

    show processing status for saxs

--- client/src/js/modules/types/saxs/dc/dc.js
+++ client/src/js/modules/types/saxs/dc/dc.js
@@ -1,23 +1,32 @@
 define([
     'modules/types/gen/dc/dc',
+    'modules/dc/views/apstatusitem',
+    'modules/dc/views/downstream',
     'modules/types/saxs/dc/datplot',
     'utils',
-    'templates/types/saxs/dc/dc.html'], function(DCItemView, DatPlot, utils, template) {
+    'templates/types/saxs/dc/dc.html'], function(DCItemView, APStatusItem, DCDownstreamView, DatPlot, utils, template) {
 
 
     return DCItemView.extend({
         template: template,
         plotView: DatPlot,
-        // imageStatusItem: DCImageStatusItem,
+        apStatusItem: APStatusItem,
         
         events: {
             'click .diffraction': 'showDiff',
             'click a.dd': utils.signHandler,
+            'click .holder h1.dp': 'loadAP',
         },
         
         showDiff: function(e) {
             e.preventDefault()
             this.$el.find('.diffraction a').eq(0).trigger('click')
+        },
+
+        loadAP: function(e) {
+            if (!this.ap) {
+              this.ap = new DCDownstreamView({ id: this.model.get('ID'), el: this.$el.find('div.downstream') })
+            } else this.ap.$el.slideToggle()
         },
         
     })
