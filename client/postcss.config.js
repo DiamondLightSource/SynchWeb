e424a470ab6d2f785d962b6a18af9dea96d512fd
Neil Smith <neil.smith@diamond.ac.uk>
2021-04-12 14:18:52 +0100

    Disable purgecss for production builds

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,16 +1,18 @@
 // postcss.config.js
 const path = require('path');
-const purgecss = require('@fullhuman/postcss-purgecss')({
+// Disabling purgecss for now because it strips too many styles.
+// TODO - figure out the correct extractor to parse backbone templates (inline and html)
+// const purgecss = require('@fullhuman/postcss-purgecss')({
 
-  // Specify the paths to all of the template files in your project
-  content: [
-    './src/index.php',
-    './src/**/*.html',
-    './src/**/*.vue',
-    './src/**/*.js', /* Lots of classes are used within inline templates... */
-  ],
-  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-})
+//   // Specify the paths to all of the template files in your project
+//   content: [
+//     './src/index.php',
+//     './src/**/*.html',
+//     './src/**/*.vue',
+//     './src/**/*.js', /* Lots of classes are used within inline templates... */
+//   ],
+//   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+// })
 
 module.exports = ({env}) => (
   {
@@ -29,7 +31,8 @@ module.exports = ({env}) => (
       require('postcss-extend-rule'),
       require('tailwindcss'),
       require('autoprefixer'),
-      ...process.env.NODE_ENV === 'production' ? [purgecss]: [],
+      // Disabling purgecss for now - strips out too many styles
+      // ...process.env.NODE_ENV === 'production' ? [purgecss]: [],
     ]
   }
 )
