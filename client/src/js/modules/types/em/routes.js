1c95b50df5ff359c3c51f32f19da1b739878336c
Neil A Smith <neil.smith@diamond.ac.uk>
2021-04-15 10:16:53 +0100

    Add missing EM routers for relion/scipion processing

---
+++ client/src/js/modules/types/em/routes.js
@@ -0,0 +1,53 @@
+import MarionetteView from 'app/views/marionette/marionette-wrapper.vue'
+
+import RelionView from 'modules/types/em/relion/views/relion'
+import ScipionView from 'modules/types/em/scipion/views/scipion'
+
+const routes = [
+    {
+        path: '/em/process/relion/session/:session_str',
+        component: MarionetteView,
+        props: route => ({
+            mview: RelionView,
+            options: {
+                session_str: session_str
+            },
+            breadcrumbs: [{ title: 'Relion Processing' }, { title: session_str }]
+        }),
+        beforeEnter: (to, from, next) => {
+            // Copying the logic from types/em/relion/controller.js
+            if (to.params.session_str) {
+                app.cookie(to.params.session_str.split('-')[0]);
+                next()
+            } else {
+                // This path should never be entered. If there is no session_str then this path will not match
+                app.message({title: 'Visit not specified', message: 'No visit specified'})
+                next('/notfound')
+            }
+        }
+    },
+    {
+        path: '/em/process/scipion/visit/:visit_str',
+        component: MarionetteView,
+        props: route => ({
+            mview: ScipionView,
+            options: {
+                visit_str: visit_str
+            },
+            breadcrumbs: [{ title: 'Scipion Processing' }, { title: visit_str }]
+        }),
+        beforeEnter: (to, from, next) => {
+            // Copying the logic from types/em/scipion/controller.js
+            if (to.params.visit_str) {
+                app.cookie(to.params.visit_str.split('-')[0]);
+                next()
+            } else {
+                // This path should never be entered. If there is no session_str then this path will not match
+                app.message({title: 'Visit not specified', message: 'No visit specified'})
+                next('/notfound')
+            }
+        }
+    }
+]
+
+export default routes
