d309dbfa688408c2092c25f0779e78b1375202f8
Andy Preston <edgeeffect@gmail.com>
2021-06-25 13:16:08 +0100

    shared component for all the auto processing images

---
+++ client/src/js/modules/types/em/dc/views/components/dc-image.vue
@@ -0,0 +1,54 @@
+<template>
+  <div
+    :class="classes"
+    :title="containerTitle"
+    :style="style"
+  >
+    <a
+      :href="imageUrl"
+      :title="imageTitle"
+    >
+      <img :alt="imageTitle">
+    </a>
+  </div>
+</template>
+
+<script>
+import 'jquery.mp'
+
+export default {
+    'name': "DcImage",
+    'props': {
+        'classes': {
+            'type': String,
+            'required': true
+        },
+        'containerTitle': {
+            'type': String,
+            'required': true,
+        },
+        'imageUrl': {
+            'type': String,
+            'required': true,
+        },
+        'imageTitle': {
+            'type': String,
+            'required': true,
+        },
+    },
+    'computed': {
+        'style': function() {
+            // TODO: Find a tidier way to apply the scaling factor
+            const windowWidth = $(window).width()
+            const proportionalHeight = 0.175 * windowWidth * 0.95
+            const heightScalingFactor = windowWidth < 800 ? 1.65 : (
+                windowWidth < 1280 ? 1.3 : 1
+            )
+            const height = Math.round(
+                proportionalHeight * heightScalingFactor * 0.8
+            ) + 'px;'
+            return 'min-height: ' + height + ' max-height: ' + height
+        },
+    },
+}
+</script>
--- client/src/js/modules/types/em/dc/views/ctf-fft.vue
+++ client/src/js/modules/types/em/dc/views/ctf-fft.vue
@@ -1,26 +1,20 @@
 <template>
-  <div
-    class="diffraction fft"
-    title="Click to view FFT"
-    :style="divStyle"
-  >
-    <a
-      :href="hrefUrl"
-      title="FFT Theoretical"
-    >
-      <img
-        src=""
-        alt="FFT Theoretical"
-      >
-    </a>
-  </div>
+  <dc-image
+    container-title="Click to view FFT"
+    image-title="FFT Theoretical"
+    classes="diffraction fft"
+    :image-url="imageUrl"
+  />
 </template>
 
 <script>
-import 'jquery.mp'
+import dcImage from 'modules/types/em/dc/views/components/dc-image.vue'
 
 export default {
     'name': "CtfFftView",
+    'components': {
+        'dc-image': dcImage
+    },
     'props': {
         'apiUrl': {
             'type': String,
@@ -32,31 +26,17 @@ export default {
         },
     },
     'computed': {
-        'hrefUrl': function() {
+        'imageUrl': function() {
             return this.apiUrl +
-                '/em/ctf/image/' +
-                this.model.get('DATACOLLECTIONID') +
-                '/n/' +
-                this.model.get('IMAGENUMBER')
+                '/em/ctf/image/' + this.model.get('DATACOLLECTIONID') +
+                '/n/' + this.model.get('IMAGENUMBER')
         },
         /* TODO: There is no data that includes a FftTheoreticalFullPath
                  since 2019.
                  I'm assuming that this image is not in use.
-                 If you do need to "port" the loading code, go back to
-                 before this commit and look at modules/types/em/dc/views/ctf.js
+                 If you do need to "port" the loading code, see:
+                 https://github.com/DiamondLightSource/SynchWeb/blob/master/client/src/js/modules/types/em/dc/views/ctf.js
         */
-        'divStyle': function() {
-            // TODO: Find a tidier way to apply the scaling factor
-            const windowWidth = $(window).width()
-            const proportionalHeight = 0.175 * windowWidth * 0.95
-            const heightScalingFactor = windowWidth < 800 ? 1.65 : (
-                windowWidth < 1280 ? 1.3 : 1
-            )
-            const height = Math.round(
-                proportionalHeight * heightScalingFactor * 0.8
-            ) + 'px;'
-            return 'min-height: ' + height + ' max-height: ' + height
-        },
     },
 }
 </script>
