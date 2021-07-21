794ce684f94d60943507f2b3a268c136aadff085
Andy Preston <edgeeffect@gmail.com>
2021-07-21 14:36:38 +0100

    [SCI-9840][SCI-9842] Integrating MORE Motion Control into new page layout

--- client/src/js/modules/types/em/dc/ap/mc/motion-correction.vue
+++ client/src/js/modules/types/em/dc/ap/mc/motion-correction.vue
@@ -1,153 +1,190 @@
 <template>
   <div class="mc dcap clearfix">
-    <p v-if="nothingToShow">
-      No Motion Correction for this movie
+    <h2>Motion Correction</h2>
+
+    <movie-select
+      :max="length"
+      @changed="newMovie"
+    />
+
+    <p class="ra">
+
+      <a
+        class="view button logf"
+        :href="logUrl"
+        @click.prevent="showLog"
+      ><i class="fa fa-search" /> Log file</a>
     </p>
 
-    <template v-else>
-      <h2>Motion Correction</h2>
+    <div
+      class="data_collection"
+      type="data"
+    >
+      <drift
+        :data-collection-id="dataCollectionId"
+        :movie-number="movieNumber"
+      />
 
-      <dialog-box />
+      <!-- FFT of Motion Corrected Image -->
+      <dc-image
+        container-class="diffraction fft2"
+        container-title="Click to view FFT"
+        :image-url="fftUrl"
+        image-title="FFT Drift Corrected"
+      />
 
-      <p class="ra">
-        <!--
-          a
-          class="dll button"
-          :href="dllUrl"
-          @click.prevent="showOutput"
-        ><i class="fa fa-download" /> Micrograph</a -->
-        <a
-          class="view button logf"
-          :href="logUrl"
-          @click.prevent="showLog"
-        ><i class="fa fa-search" /> Log file</a>
-      </p>
+      <!-- Motion Corrected Image -->
+      <dc-image
+        container-class="diffraction fft"
+        container-title="Click to view drift corrected FFT"
+        :image-url="imageUrl"
+        image-title="FFT"
+      />
 
-      <div
-        class="data_collection"
-        type="data"
-      >
-        <drift />
-
-        <!-- FFT of Motion Corrected Image -->
-        <dc-image
-          container-class="diffraction fft2"
-          container-title="Click to view FFT"
-          :image-url="fftUrl"
-          image-title="FFT Drift Corrected"
-        />
-
-        <!-- Motion Corrected Image -->
-        <dc-image
-          container-class="diffraction fft"
-          container-title="Click to view drift corrected FFT"
-          :image-url="imageUrl"
-          image-title="FFT"
-        />
-
-        <params />
-      </div>
-    </template>
+      <params
+        v-if="motionCorrection !== null"
+        :motion-correction="motionCorrection"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import store from 'app/store/store'
-import LogView from 'views/log'
+import DcImage from 'modules/types/em/components/dc-image.vue'
+import Drift from 'modules/types/em/dc/ap/mc/drift.vue'
+import LogView from 'views/log' // TODO: needs fixing or scrapping ?
+import MotionCorrectionModel from 'modules/types/em/models/motioncorrection'
+import MovieSelect from 'modules/types/em/components/movie-select.vue'
+import Params from 'modules/types/em/dc/ap/mc/params.vue'
 import utils from 'utils'
-/* TODO: dialogbox.vue belongs on a top level page
-   keep moving it until the whole page is "vue-ified" */
-import DialogBox from 'app/components/dialogbox.vue'
-import DcImage from 'modules/types/em/dc/views/dc-image.vue'
-import Params from 'modules/types/em/dc/views/motion-correction/params.vue'
-import Drift from 'modules/types/em/dc/views/motion-correction/drift.vue'
 
 export default {
     'name': "MotionCorrection",
     'components': {
-        'dialog-box': DialogBox,
         'dc-image': DcImage,
-        'params': Params,
         'drift': Drift,
+        'movie-select': MovieSelect,
+        'params': Params,
+    },
+    'props': {
+        'active' : {
+            'type': Boolean,
+            'required': true,
+        },
+        'length': {
+            'type': Number,
+            'required': true,
+        },
+        'dataCollectionId': {
+            'type': Number,
+            'required': true,
+        },
     },
     'data': function() {
         return {
-            'apiUrl': store.state.apiUrl
+            'movieNumber': 1,
+            'motionCorrection': null,
         }
     },
     'computed': {
-        /*
-        'dllUrl': function() {
-            return this.apiUrl +
-                '/download/id/' +
-                store.state.models.emMotionCorrection.DATACOLLECTIONID +
-                '/aid/' +
-                store.state.models.emMotionCorrection.AUTOPROCPROGRAMID
+        'motionCorrectionModel': function() {
+            return new MotionCorrectionModel({
+                'id': this.dataCollectionId,
+                'TYPE': 'Motion Correction',
+            })
         },
-        */
+        'loadedMovieNumber': function() {
+            return this.motionCorrection === null ?
+                null : this.motionCorrection.IMAGENUMBER
+        },
         'logUrl': function() {
-            return this.apiUrl +
-                '/download/id/' +
-                store.state.models.emMotionCorrection.DATACOLLECTIONID +
-                '/aid/' +
-                store.state.models.emMotionCorrection.AUTOPROCPROGRAMID +
+            if (this.motionCorrection === null) {
+                return '#'
+            }
+            return this.$store.state.apiUrl +
+                '/download/id/' + this.dataCollectionId +
+                '/aid/' + this.motionCorrection.AUTOPROCPROGRAMID +
                 '/log/1'
         },
         'fftUrl': function() {
-            return this.apiUrl +
-                '/em/mc/fft/image/' +
-                store.state.models.emMotionCorrection.DATACOLLECTIONID +
-                '/n/' +
-                store.state.models.emMotionCorrection.IMAGENUMBER +
+            return this.$store.state.apiUrl +
+                '/em/mc/fft/image/' + this.dataCollectionId +
+                '/n/' + this.movieNumber +
                 '/t/2'
         },
         'imageUrl': function() {
-            return this.apiUrl +
-                '/em/mc/fft/image/' +
-                store.state.models.emMotionCorrection.DATACOLLECTIONID +
-                '/n/' +
-                store.state.models.emMotionCorrection.IMAGENUMBER
+            return this.$store.state.apiUrl +
+                '/em/mc/fft/image/' + this.dataCollectionId +
+                '/n/' + this.movieNumber
         },
-        'nothingToShow': function() {
-            return !store.state.models.emMotionCorrection.MOTIONCORRECTIONID
+    },
+    'watch': {
+        // eslint-disable-next-line no-unused-vars
+        'active': function(newValue, oldValue) {
+            this.fetchMovie()
         },
-        'collectionId': function() {
-            return store.state.models.emMotionCorrection.id
-        },
-        'imageNumber': function() {
-            return store.state.models.emMotionCorrection.IMAGENUMBER
+        // eslint-disable-next-line no-unused-vars
+        'movieNumber': function(newValue, oldValue) {
+            this.fetchMovie()
         },
     },
     'methods': {
-        'signUrl': function(rootUrl, actionCallback) {
+        'newMovie': function(movieNumber) {
+            this.movieNumber = movieNumber
+        },
+        'fetchMovie': function() {
+            const vm = this
+            if (vm.active == false || vm.movieNumber == vm.loadedMovieNumber) {
+                return
+            }
+            vm.$store.commit('loading', true)
+            const successCallback = function(
+                model, // eslint-disable-line no-unused-vars
+                response,
+                options // eslint-disable-line no-unused-vars
+            ) {
+                vm.$store.commit('loading', false)
+                vm.motionCorrection = response
+            }
+            const errorCallback = function (
+                model, // eslint-disable-line no-unused-vars
+                response,
+                options // eslint-disable-line no-unused-vars
+            ) {
+                vm.$store.commit('loading', false)
+                console.log(response.responseJSON)
+                vm.$store.commit('notifications/addNotification', {
+                    'title': 'Error',
+                    'message': 'Could not retrieve motion correction data',
+                    'level': 'error'
+                })
+            }
+            vm.$store.commit('loading', true)
+            /* TODO: [SCI-9935]
+               vm.$store.dispatch('getModel', model)
+               doesn't currently support 'data': */
+            vm.motionCorrectionModel.fetch({
+                'data': { 'IMAGENUMBER': vm.movieNumber },
+                'success': successCallback,
+                'error': errorCallback,
+            })
+        },
+        'showLog': function() {
+            const url = this.logUrl
             utils.sign({
-                /* TODO: when testing, I'm getting "401 Unauthorised"
-                   {"error":"No authorisation token provided"} */
-                'url': rootUrl,
+                'url': url,
                 'callback': function(response) {
-                    actionCallback(rootUrl + '?token=' + response.token)
-                }
+                    const signedUrl = url + '?token=' + response.token
+                    // TODO: Dialog broken!
+                    MarionetteApplication.getInstance().dialog.show(
+                        new LogView({
+                            'title': 'Motion Correction Log File',
+                            'url': signedUrl,
+                        })
+                    )
+                },
             })
         },
-        'showLog': function(event) {
-            /* TODO: The Log Button is essentially untested
-              All examples I can find give a Blank dialog for this file */
-            const type = store.state.models.emMotionCorrection.TYPE
-            this.signUrl(event.target.href, function(signedUrl) {
-                // TODO: The dialog doesn't fit nicely on the screen
-                MarionetteApplication.getInstance().dialog.show(new LogView({
-                    'title': type + ' Log File',
-                    'url': signedUrl
-                }))
-            })
-        },
-        /*
-        'showOutput': function(event) {
-            this.signUrl(event.target.href, function(signedUrl) {
-                window.location = signedUrl
-            })
-        },
-        */
     },
 }
 </script>
