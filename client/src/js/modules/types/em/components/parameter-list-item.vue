52e05257637dfdc5f7930e9e0ac6b4b22e48d324
Andy Preston <edgeeffect@gmail.com>
2021-08-02 12:04:28 +0100

    [SCI-9840] Improved Parameter lists Vue components for EM

---
+++ client/src/js/modules/types/em/components/parameter-list-item.vue
@@ -0,0 +1,50 @@
+<template>
+  <custom-list-item
+    helpText="helpText"
+    :width="width"
+  >
+    {{ label }}:
+    <span v-html="item" />
+    <template v-if="label2">
+      {{ label2 }}:
+      <span v-html="item2" />
+    </template>
+  </custom-list-item>
+</template>
+
+<script>
+import CustomListItem from 'modules/types/em/components/custom-list-item.vue'
+
+export default {
+    'name': 'ParameterListItem',
+    'components': {
+        'custom-list-item': CustomListItem
+    },
+    'props': {
+        'width': {
+            'type': String,
+            'default': '50%',
+        },
+        'label': {
+            'type': String,
+            'default': '',
+        },
+        'item': {
+            'type': String,
+            'required': true,
+        },
+        'label2': {
+            'type': String,
+            'default': '',
+        },
+        'item2': {
+            'type': String,
+            'default': '',
+        },
+        'helpText': {
+            'type': String,
+            'default': '',
+        },
+    },
+}
+</script>
