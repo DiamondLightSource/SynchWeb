d2e7aaa25f2c2c90a350a201686a2808c520e61b
Andy Preston <edgeeffect@gmail.com>
2021-08-02 12:39:37 +0100

    [SCI-9873][SCI-9840] processing job status description

---
+++ client/src/js/modules/types/em/dc/ap/status-description.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    {{ status }}
+    <i
+      class="fa icon"
+      :alt="status"
+      :class="icon"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+    'name': 'StatusDescription',
+    'props': {
+        'status': {
+            'type': String,
+            'required': true,
+        },
+    },
+    'computed': {
+        'icon': function() {
+            return {
+                'submitted': 'grey fa-cog fa-spin', // TODO
+                'queued': 'grey fa-cog fa-spin', // TODO
+                'running': 'grey fa-cog fa-spin',
+                'failure': 'red fa-times',
+                'success': 'green fa-check',
+                '': 'blue fa-question-circle',
+            }[this.status]
+        },
+    },
+}
+</script>
