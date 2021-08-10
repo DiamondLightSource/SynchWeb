aecd430a20042e5498a1cacb45036591353882f5
Andy Preston <edgeeffect@gmail.com>
2021-08-10 17:52:32 +0100

    base-input-button has "normal", "success", "danger"

--- client/src/js/app/components/base-input-button.vue
+++ client/src/js/app/components/base-input-button.vue
@@ -1,6 +1,7 @@
 <template>
   <button
     :disabled="disabled"
+    :class="type"
     @click="$emit('click')"
   >
     <slot />
@@ -14,29 +15,63 @@ export default {
         'disabled': {
             'type': Boolean,
             'default': false,
-        }
+        },
+        'type': {
+            'type': String,
+            'default': 'normal',
+            'validator': function(value) {
+                return [
+                    'normal',
+                    'success',
+                    'danger',
+                ].indexOf(value) !== -1
+            },
+        },
     },
 }
 </script>
 
 <style scoped>
 button {
-    border-width: 1px;
+    /*border-width: 1px;
     border-style: solid;
-    border-color: #e2e2e2;
+    border-color: #e2e2e2;*/
     padding: 5px;
     border-radius: 3px;
+}
+.normal {
     background: linear-gradient(#ededed, #dedede);
     color: #666;
 }
-
-button:hover:enabled {
+.normal:hover:enabled {
     background: linear-gradient(#dedede, #ededed);
     color: #000;
 }
+.normal:disabled, .normal[disabled] {
+    color: #bbb;
+}
 
-button:disabled, button[disabled] {
-  background-color: #ddd;
-  color: #bbb;
+.success {
+    background: linear-gradient(#82d180, #28a745);
+    color: #555;
+}
+.success:hover:enabled {
+    background: linear-gradient(#28a745, #82d180);
+    color: #fff;
+}
+.success:disabled, .success[disabled] {
+  color: #888;
+}
+
+.danger {
+    background: linear-gradient(#ef4444, #dc3545);
+    color: #555;
+}
+.danger:hover:enabled {
+    background: linear-gradient(#dc3545, #ef4444);
+    color: #fff;
+}
+.danger:disabled, .danger[disabled] {
+  color: #999;
 }
 </style>
