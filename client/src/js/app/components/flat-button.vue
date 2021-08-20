b29d752be5f0a95c023d2835a52900e1577307b7
Andy Preston <edgeeffect@gmail.com>
2021-08-20 16:53:25 +0100

    tidied up styling of flat button and added "hint" property

--- client/src/js/app/components/flat-button.vue
+++ client/src/js/app/components/flat-button.vue
@@ -2,7 +2,10 @@
   <button
     class="flat-button"
     :class="levelToClasses"
-    @click="$emit('click')"
+    :title="hint"
+    @click.prevent="$emit('click')"
+    @mouseover.prevent="$emit('mouseover')"
+    @mouseleave="$emit('mouseleave')"
   >
     <slot />
   </button>
@@ -16,28 +19,39 @@ export default {
             'type': String,
             'default': 'secondary',
         },
+        'hint': {
+            'type': String,
+            'default': '',
+        },
     },
     'data': function() {
         return {
             'colours': {
-                'primary': ['white', 'blue', 500],
-                'secondary': ['white', 'gray', 500],
-                'success': ['white', 'green', 500],
+                'primary': ['blue', 500, 'white'],
+                'secondary': ['gray', 400, 'black', 'white'],
+                'success': ['green', 500, 'white'],
                 // Cyan would be usual for info, but Tailwind has no such thing
-                'info': ['black', 'pink', 500],
-                'warning': ['black', 'yellow', 300],
-                'danger': ['white', 'red', 500],
+                'info': ['pink', 500, 'black'],
+                'warning': ['yellow', 300, 'black'],
+                'danger': ['red', 500, 'white'],
             },
         }
     },
     'computed': {
         'levelToClasses': function() {
-            const [foreground, background, shade] =
+            const [background, shade, foreground, foregroundHover] =
                 this.colours[this.level]
-            const darkened = 'tw-bg-' + background + '-' + (shade + 200)
-            return 'tw-text-' + foreground +
-                ' tw-bg-' + background + '-' + shade +
-                ' hover:' + darkened + ' focus:' + darkened
+            var classes = [
+                'tw-text-' + foreground,
+                'tw-bg-' + background + '-' + shade,
+                'hover:tw-bg-' + background + '-' + (shade + 200),
+                'focus:tw-bg-' + background + '-' + (shade + 200),
+            ]
+            if (foregroundHover) {
+                classes.push('hover:tw-text-' + foregroundHover)
+                classes.push('focus:tw-text-' + foregroundHover)
+            }
+            return classes.join(' ')
         }
     }
 }
