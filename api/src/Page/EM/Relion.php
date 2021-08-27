d3d3b40e9ca06a7ab91d8c1a3294dd270a4d0748
Andy Preston <edgeeffect@gmail.com>
2021-08-27 19:00:38 +0100

    Split more Relion endpoint code into it's own Trait

--- api/src/Page/EM/Relion.php
+++ api/src/Page/EM/Relion.php
@@ -9,8 +9,8 @@ trait Relion
 {
     public function relionStart()
     {
-        global $visit_directory/*,
-            $zocalo_mx_reprocess_queue*/;
+        global $visit_directory,
+            $zocalo_mx_reprocess_queue;
 
         $this->exitIfElectronMicroscopesAreNotConfigured();
         $session = $this->determineSession($this->arg('session'));
@@ -81,36 +81,6 @@ trait Relion
         $this->_output($output);
     }
 
-    private function findExistingDataCollection(
-        $session,
-        $imageDirectory,
-        $fileTemplate
-    ) {
-        // Returns dataCollectionId of first DataCollection associated with session
-        // Also checks for existing imageDirectory
-        // Returns null otherwise
-
-        if ($session['SESSIONID']) {
-            $result = $this->db->pq(
-                "SELECT dataCollectionId FROM DataCollection
-                    WHERE SESSIONID = :1 AND imageDirectory = :2 AND fileTemplate = :3
-                    LIMIT 1",
-                array(
-                    $session['SESSIONID'],
-                    $imageDirectory,
-                    $fileTemplate
-                )
-            );
-            if (count($result)) {
-                return $result[0]['DATACOLLECTIONID'];
-            }
-        }
-
-        return null;
-    }
-
-
-
     public function relionStatus()
     {
         // global $visit_directory;
@@ -133,7 +103,7 @@ trait Relion
 
     public function relionStop()
     {
-        // global $zocalo_mx_reprocess_queue;
+        global $zocalo_mx_reprocess_queue;
 
         // $session = $this->determineSession($this->arg('session'));
         // $this->exitIfSessionIsNotActive($session);
@@ -191,5 +161,156 @@ trait Relion
         }
 
         $this->_output(array('data' => $parameters, 'total' => sizeof($parameters)));
+    }
+
+    ////////////////////////////////////////////////////////////////////////////
+
+    private function findExistingDataCollection(
+        $session,
+        $imageDirectory,
+        $fileTemplate
+    ) {
+        // Returns dataCollectionId of first DataCollection associated with session
+        // Also checks for existing imageDirectory
+        // Returns null otherwise
+
+        if ($session['SESSIONID']) {
+            $result = $this->db->pq(
+                "SELECT dataCollectionId FROM DataCollection
+                    WHERE SESSIONID = :1 AND imageDirectory = :2 AND fileTemplate = :3
+                    LIMIT 1",
+                array(
+                    $session['SESSIONID'],
+                    $imageDirectory,
+                    $fileTemplate
+                )
+            );
+            if (count($result)) {
+                return $result[0]['DATACOLLECTIONID'];
+            }
+        }
+
+        return null;
+    }
+
+    private function exitIfUnfinishedProcessingJobsExist($session)
+    {
+        // Finds queued and running ProcessingJobs associated with session
+        // Returns null otherwise
+
+        if ($session['SESSIONID']) {
+            $result = $this->db->pq(
+                "SELECT
+                    APP.autoProcProgramId,
+                    APP.processingStartTime,
+                    APP.processingJobId,
+                    CASE
+                        WHEN (processingStartTime IS NULL AND processingEndTime IS NULL AND processingStatus IS NULL) THEN 'queued'
+                        WHEN (processingStartTime IS NOT NULL AND processingEndTime IS NULL AND processingStatus IS NULL) THEN 'running'
+                    END AS processingStatusDescription
+                FROM AutoProcProgram APP
+                JOIN ProcessingJob PJ ON PJ.processingJobId = APP.processingJobId
+                JOIN DataCollection DC ON PJ.dataCollectionId = DC.dataCollectionId
+                JOIN BLSession BLS ON DC.SESSIONID = BLS.sessionId
+                WHERE processingStatus IS NULL
+                  AND BLS.sessionId = :1",
+                array($session['SESSIONID'])
+            );
+
+            if (count($result)) {
+                $message = 'Relion processing job already exists for this session!';
+
+                error_log($message);
+                $this->_error($message, 400);
+
+                return $result;
+            }
+        }
+
+        return null;
+    }
+
+    private function addDataCollectionForEM($session, $imageDirectory, $imageSuffix, $fileTemplate)
+    {
+        $dataCollectionId = null;
+
+        try {
+            $this->db->start_transaction();
+
+            // Add DataCollectionGroup
+
+            $this->db->pq(
+                "INSERT INTO DataCollectionGroup (sessionId, comments, experimentType)
+                VALUES (:1, :2, :3) RETURNING dataCollectionGroupId INTO :id",
+                array($session['SESSIONID'], 'Created by SynchWeb', 'EM')
+            );
+
+            $dataCollectionGroupId = $this->db->id();
+
+            // Add DataCollection
+
+            $this->db->pq(
+                "INSERT INTO DataCollection (sessionId, dataCollectionGroupId, startTime,
+                    endTime, runStatus, imageDirectory, imageSuffix, fileTemplate, comments)
+                VALUES (:1, :2, NOW(), :3, :4, :5, :6, :7, :8) RETURNING dataCollectionId INTO :id",
+                array(
+                    $session['SESSIONID'],
+                    $dataCollectionGroupId,
+                    $session['ENDDATE'],
+                    'DataCollection Simulated',
+                    $imageDirectory,
+                    $imageSuffix,
+                    $fileTemplate,
+                    'Created by SynchWeb'
+                )
+            );
+
+            $dataCollectionId = $this->db->id();
+
+            $this->db->end_transaction();
+        } catch (Exception $e) {
+            error_log("Failed to add DataCollection to database.");
+            $this->_error("Failed to add DataCollection to database.", 500);
+        }
+
+        return $dataCollectionId;
+    }
+
+    private function addProcessingJobForRelion($dataCollectionId, $workflowParameters)
+    {
+        $processingJobId = null;
+
+        try {
+            $this->db->start_transaction();
+
+            // Add ProcessingJob
+
+            $this->db->pq(
+                "INSERT INTO ProcessingJob (dataCollectionId, displayName, comments, recipe, automatic)
+                VALUES (:1, :2, :3, :4, :5) RETURNING processingJobId INTO :id",
+                array($dataCollectionId, 'RELION', 'Submitted via SynchWeb', 'relion', 0)
+            );
+
+            $processingJobId = $this->db->id();
+
+            // Add ProcessingJobParameters
+
+            foreach ($workflowParameters as $key => $value) {
+                $this->db->pq(
+                    "INSERT INTO ProcessingJobParameter (processingJobId, parameterKey, parameterValue)
+                    VALUES (:1, :2, :3)",
+                    array($processingJobId, $key, (is_bool($value) ? var_export($value, true) : $value)) // TODO REMOVE QUOTES FROM STRINGS
+                );
+            }
+
+//            array($processingJobId, $key, var_export($value, true)) // TODO REMOVE QUOTES FROM STRINGS
+
+            $this->db->end_transaction();
+        } catch (Exception $e) {
+            error_log("Failed to add ProcessingJob to database.");
+            $this->_error("Failed to add ProcessingJob to database.", 500);
+        }
+
+        return $processingJobId;
     }
 }
