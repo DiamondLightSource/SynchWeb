43a85d8c8fed2cf1c2f062e6249b84be0243be98
Andy Preston <edgeeffect@gmail.com>
2021-09-07 11:15:55 +0100

    [SCI-10025] Particle picker frontend

---
+++ client/src/js/modules/types/em/picker/params.vue
@@ -0,0 +1,48 @@
+<template>
+  <parameter-list>
+    <parameter-list-item
+      label="Movie Number"
+      :item="movieNumber"
+    />
+    <parameter-list-item
+      label="Number of Particles"
+      :item="numberOfParticles"
+    />
+    <parameter-list-item
+      label="Particle Diameter"
+      :item="particleDiameter"
+    />
+  </parameter-list>
+</template>
+
+<script>
+import parameterFormats from 'modules/types/em/components/parameter-formats'
+import ParameterListItem from 'modules/types/em/components/parameter-list-item.vue'
+import ParameterList from 'modules/types/em/components/parameter-list.vue'
+
+export default {
+    'name': "Params",
+    'components': {
+        'parameter-list-item': ParameterListItem,
+        'parameter-list': ParameterList,
+    },
+    'mixins': [parameterFormats],
+    'props': {
+        'pick': {
+            'type': Object,
+            'required': true,
+        },
+    },
+    'computed': {
+        'movieNumber': function() {
+            return this.datumOrBlank(this.pick.movieNumber)
+        },
+        'numberOfParticles': function() {
+            return this.datumOrBlank(this.pick.numberOfParticles)
+        },
+        'particleDiameter': function() {
+            return this.datumOrBlank(this.pick.particleDiameter)
+        },
+    },
+}
+</script>
--- client/src/js/modules/types/em/picker/picker.vue
+++ client/src/js/modules/types/em/picker/picker.vue
@@ -1,26 +1,59 @@
 <template>
-  <processing-section
+  <processing-section-movie-list
     section-title="Particle Picking"
-    :data-available="true"
+    url-prefix="/em/picker/"
+    :auto-proc-program-id="autoProcProgramId"
+    :loaded-movie-number="loadedMovieNumber"
+    @loaded="loaded"
   >
-    <div>
-      TODO:
-    </div>
-  </processing-section>
+    <params :pick="pick" />
+
+    <dialog-image
+      title="Summary Image"
+      :image-url="imageUrl"
+    />
+  </processing-section-movie-list>
 </template>
 
 <script>
-import ProcessingSection from 'modules/types/em/components/processing-section.vue'
+import DialogImage from 'modules/types/em/components/dialog-image.vue'
+import ProcessingSectionMovieList from 'modules/types/em/components/processing-section-movie-list.vue'
+import Params from 'modules/types/em/picker/params.vue'
 
 export default {
     'name': "Picker",
     'components': {
-        'processing-section': ProcessingSection,
+        'dialog-image': DialogImage,
+        'params': Params,
+        'processing-section-movie-list': ProcessingSectionMovieList,
     },
     'props': {
         'autoProcProgramId': {
             'type': Number,
             'required': true,
+        },
+    },
+    'data': function() {
+        return {
+            'pick': {},
+        }
+    },
+    'computed': {
+        'loadedMovieNumber': function() {
+            return 'movieNumber' in this.pick ? this.pick.movieNumber : ''
+        },
+        'imageUrl': function() {
+            const movie = this.loadedMovieNumber
+            if (movie == '') {
+                return '#'
+            }
+            return this.$store.state.apiUrl +
+                '/em/picker/image/' + this.autoProcProgramId + '/n/' + movie
+        },
+    },
+    'methods': {
+        'loaded': function(pick) {
+            this.pick = pick
         },
     },
 }
