25e3ee81b1948796c5685c26b8304cdf3fcc08ce
Scott Classen <dsclassen@users.noreply.github.com>
2021-09-16 10:33:44 -0700

    Update LDAP.php
    add ldap_start_tls

--- api/src/Authentication/Type/LDAP.php
+++ api/src/Authentication/Type/LDAP.php
@@ -22,6 +22,7 @@ class LDAP extends AuthenticationParent implements AuthenticationInterface
         if ($conn) {
             // Tested against LDAP version 3 (could add support for older versions here)
             ldap_set_option($conn, LDAP_OPT_PROTOCOL_VERSION, 3);
+            ldap_start_tls($conn);
 
             try {
                 // testing with openldap indicates this call needs to use a correct
