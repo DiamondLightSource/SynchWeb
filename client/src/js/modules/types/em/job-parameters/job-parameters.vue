57929267e15084c8cf315d4aba875fe1be603203
Andy Preston <nfx92172@pw050.diamond.ac.uk>
2021-10-06 16:03:34 +0100

    Fixed job-parameters to work with updated processing-section-with-schema

--- client/src/js/modules/types/em/job-parameters/job-parameters.vue
+++ client/src/js/modules/types/em/job-parameters/job-parameters.vue
@@ -1,20 +1,26 @@
 <template>
-  <processing-section-with-schema
+  <processing-section
     section-title="Processing Job Parameters"
-    schema-name="Relion Schema"
-    schema-url="relion/schema/"
-    :data-url="dataUrl"
+    :data-available="Object.keys(parameters).length > 0"
     default-hidden
-  />
+  >
+    <parameter-list-with-schema
+      schema-name="Relion Schema"
+      schema-url="relion/schema/"
+      :parameters="parameters"
+    />
+  </processing-section>
 </template>
 
 <script>
-import ProcessingSectionWithSchema from 'modules/types/em/components/processing-section-with-schema.vue'
+import ProcessingSection from 'modules/types/em/components/processing-section.vue'
+import ParameterListWithSchema from 'modules/types/em/components/parameter-list-with-schema.vue'
 
 export default {
     'name': 'JobParameters',
     'components': {
-        'processing-section-with-schema': ProcessingSectionWithSchema,
+        'processing-section': ProcessingSection,
+        'parameter-list-with-schema': ParameterListWithSchema,
     },
     'props': {
         'processingJobId': {
@@ -22,11 +28,19 @@ export default {
             'required': true,
         },
     },
-    'computed': {
-        'dataUrl': function() {
-            return 'relion/parameters?processingJobId=' +
-                this.processingJobId
-        },
+    'data': function() {
+        return {
+            'parameters': {},
+        }
+    },
+    'mounted': function() {
+        this.$store.dispatch('em/fetch', {
+            'url': 'relion/parameters?processingJobId=' +
+                this.processingJobId,
+            'humanName': 'Processing Job Parameters',
+        }).then(
+            (response) => { this.parameters = response }
+        )
     },
 }
 </script>
