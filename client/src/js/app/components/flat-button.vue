f69fab04685d8be1ddca0df5502c79060ae31a18
Andy Preston <nfx92172@pw050.diamond.ac.uk>
2021-10-19 14:37:40 +0100

    [SCI-10095] flat-button using Tailwind colour names

--- client/src/js/app/components/flat-button.vue
+++ client/src/js/app/components/flat-button.vue
@@ -27,8 +27,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-/* I'm using "tailwind compatible" colours here
-   ...and if you can reproduce it using actual Tailwind please feel free */
 .flat-button {
     text-align: center;
     vertical-align: middle;
@@ -41,47 +39,40 @@ export default {
         cursor: auto;
     }
     &.primary {
-        background-color: #4299e1; /* tw-bg-blue-500 */
-        color: #fff; /* tw-text-white */
+        @apply tw-bg-blue-500 tw-text-white;
         &:hover, &:focus {
-            background-color: #2b6cb0; /* tw-bg-blue-700 */
+            @apply tw-bg-blue-700;
         }
     }
     &.secondary {
-        background-color: #cbd5e0; /* tw-bg-gray-400 */
-        color: #000; /* tw-text-black */
+        @apply tw-bg-gray-400 tw-text-black;
         &:hover, &:focus {
-            background-color: #718096; /* tw-bg-gray-600 */
-            color: #fff; /* tw-text-white */
+            @apply tw-bg-gray-600 tw-text-white;
         }
     }
     &.success {
-        background-color: #48bb78; /* tw-bg-green-500 */
-        color: #fff; /* tw-text-white */
+        @apply tw-bg-green-500 tw-text-white;
         &:hover, &:focus {
-            background-color: #2f855a; /* tw-bg-green-700 */
+            @apply tw-bg-green-700;
         }
     }
     &.info {
         /* Cyan would be usual for info, but Tailwind has no such thing */
-        background-color: #ed64a6; /* tw-bg-pink-500 */
-        color: #000; /* tw-text-black */
+        @apply tw-bg-pink-500 tw-text-black;
         &:hover, &:focus {
-            background-color: #b83280; /* tw-bg-pink-700 */
+            @apply tw-bg-pink-700;
         }
     }
     &.warning {
-        background-color: #faf089; /* tw-bg-yellow-300 */
-        color: #000; /* tw-text-black */
+        @apply tw-bg-yellow-300 tw-text-black;
         &:hover, &:focus {
-            background-color: #ecc94b; /* tw-bg-yellow-500 */
+            @apply tw-bg-yellow-500;
         }
     }
     &.danger {
-        background-color: #f56565; /* tw-bg-red-500 */
-        color: #fff; /* tw-text-white */
+        @apply tw-bg-red-500 tw-text-white;
         &:hover, &:focus {
-            background-color: #c53030; /* tw-bg-red-700 */
+            @apply tw-bg-red-700
         }
     }
 }
