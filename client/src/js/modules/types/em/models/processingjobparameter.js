e8c0c2a22024776c825e976f185c0a3752e58471
Andy Preston <edgeeffect@gmail.com>
2021-10-21 17:56:59 +0100

    Outdated Backbone models and collections

--- client/src/js/modules/types/em/models/processingjobparameter.js
+++
@@ -1,6 +0,0 @@
-define(['backbone'], function (Backbone) {
-    return Backbone.Model.extend({
-        'urlRoot': '/em/process/relion/job/parameters',
-        'idAttribute': 'PROCESSINGJOBPARAMETERID',
-    })
-})
