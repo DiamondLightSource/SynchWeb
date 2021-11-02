9eafef9550486278ca809663ea4897b3fa282e53
Andy Preston <nfx92172@pw050.diamond.ac.uk>
2021-11-02 12:46:00 +0000

    Fixed errors in BoxCalculator extract_small_boxsize

--- client/src/js/modules/types/em/relion/box-calculator.js
+++ client/src/js/modules/types/em/relion/box-calculator.js
@@ -6,22 +6,6 @@
  * @param {object} parameters a reference to all of the parameters
  */
 export default function (name, parameters) {
-    const boxSizeSmall = function(extract_boxsize, angpix) {
-        const boxSizes = [
-            48, 64, 96, 128, 160, 192, 256, 288, 300, 320, 360, 384,
-            400, 420, 450, 480, 512, 640, 768, 896, 1024,
-        ];
-        for (const boxSize in boxSizes) {
-            if (boxSize > extract_boxsize) {
-                return extract_boxsize
-            }
-            if (((angpix * extract_boxsize) / boxSize) < 4.25) {
-                return boxSize
-            }
-        }
-        return "Box size is too large!";
-    }
-
     const applicableParameters = [
         'autopick_LoG_diam_max',
         'angpix',
@@ -52,9 +36,27 @@ export default function (name, parameters) {
     const boxSizeExact = particleSizePixels * 1.2
     const boxSizeInt = Math.ceil(boxSizeExact)
     const extract_boxsize = boxSizeInt + (boxSizeInt % 2)
-    const extract_small_boxsize = boxSizeSmall(extract_boxsize, angpix)
+    const extract_small_boxsize = [
+        48, 64, 96, 128, 160, 192, 256, 288, 300, 320, 360, 384,
+        400, 420, 450, 480, 512, 640, 768, 896, 1024,
+    ].reduce(
+        function(result, boxSize) {
+            if (result !== null) {
+                return result
+            }
+            if (boxSize > extract_boxsize) {
+                return extract_boxsize
+            }
+            if (((angpix * extract_boxsize) / boxSize) < 4.25) {
+                return boxSize
+            }
+            return result
+        },
+        null
+    )
 
     parameters.mask_diameter = mask_diameter.toString()
     parameters.extract_boxsize = extract_boxsize.toString()
-    parameters.extract_small_boxsize = extract_small_boxsize.toString()
+    parameters.extract_small_boxsize = extract_small_boxsize === null ?
+        'Box size is too large!' : extract_small_boxsize.toString()
 }
