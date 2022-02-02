635df6d7a11b5fd34b55508900ff8b6be076ae68
Nigel Wilson <nigel.wilson@diamond.ac.uk>
2022-02-02 15:58:32 +0000

    SCI-10142 - Updated branch to use Pods with H5Web and H5Grove, now possible to reuse a Pod within the same visit, Pod will remain active until configured delay after all tabs closed.

--- api/src/Page/Pod.php
+++ api/src/Page/Pod.php
@@ -10,20 +10,20 @@ class Pod extends Page
                                     'user' => '.*',
                             );
 
-    public static $dispatch = array(array('/maxiv/hdf5/:id', 'get', '_initiate_maxiv_pod'),
-                                    array('/maxiv/hdf5/running/:id', 'get', '_maxiv_pod_running'),
-                                    array('/maxiv/hdf5/status/:id', 'get', '_maxiv_start_status'),
+    public static $dispatch = array(array('/h5web/:id', 'get', '_initiate_h5web_pod'),
+                                    array('/h5web/running/:id', 'get', '_h5web_pod_running'),
+                                    array('/h5web/status/:id', 'get', '_h5web_start_status'),
                         );
 
     /**
      * Compile necessary parameters and send curl request to launcher application to start up a new pod
      */
-    function _initiate_maxiv_pod(){
+    function _initiate_h5web_pod(){
         $person = $this->_get_person();
 
-        // Currently we only allow users to spin up one MAXIV pod and we can't map a new file into existing Pod
+        // Currently we only allow users to spin up one H5Web pod and we can't map a new file into existing Pod
         $personHasPod = $this->db->pq("SELECT podId FROM Pod WHERE status IS NOT NULL AND status !=:1 AND STATUS !=:2 AND personId =:3", array('Terminated', 'Failed', $person));
-        if(sizeof($personHasPod) > 0) $this->_error('You have an existing instance of the MAXIV viewer running.');
+        if(sizeof($personHasPod) > 0) $this->_error('You have an existing instance of the H5Web viewer running.');
 
         $filePath = $this->_get_file_path();
         $path = $filePath['IMAGEDIRECTORY'];
@@ -32,6 +32,7 @@ class Pod extends Page
         $uid = exec("id -u ".$this->arg('user'));
 
         // Insert row acknowledging a valid pod request was sent to SynchWeb
+        // Need to update the Pod table app enum field to allow h5web and jnb (jupyter notebook)
         $this->db->pq("INSERT INTO Pod (podid, app, status, personid, filePath) 
                     VALUES (s_pod.nextval, :1, :2, :3, :4)",
                     array('MAXIV HDF5 Viewer', 'Requested', $person, $path.$file));
@@ -40,20 +41,21 @@ class Pod extends Page
         $data = array(
             'user' => $this->arg('user'),
             'userid' => $uid,
-            'IMAGEDIRECTORY' => $path,
-            'FILETEMPLATE' => $file,
-            'podid' => $podId
+            'path' => $path,
+            'file' => $file,
+            'podid' => $podId,
+            'app' => 'h5web'
         );
 
-        global $maxiv_hdf5service_url, $maxiv_hdf5service_cert;
+        global $h5web_service_url, $h5web_service_cert;
 
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $maxiv_hdf5service_url);
+        curl_setopt($ch, CURLOPT_URL, $h5web_service_url);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data, JSON_UNESCAPED_SLASHES));
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_SSLCERT, $maxiv_hdf5service_cert);
+        curl_setopt($ch, CURLOPT_SSLCERT, $h5web_service_cert);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // Blocks echo of curl response
         $result = curl_exec($ch);
         curl_close($ch);
@@ -65,7 +67,7 @@ class Pod extends Page
      * SynchWeb polls this method to check Pod status during Pod startup
      * Could be deprecated if we decide to wait for curl requests to complete (since initiate_pod will always succeed or fail)
      */
-    function _maxiv_start_status() {
+    function _h5web_start_status() {
         $podId = $this->arg('id');
         $row = $this->db->pq("SELECT status, ip, message FROM Pod where podId=:1", array($podId));
         $this->_output($row);
@@ -74,16 +76,22 @@ class Pod extends Page
     /**
      * SynchWeb polls this method to check if a Pod has terminated
      */
-    function _maxiv_pod_running() {
+    function _h5web_pod_running() {
         $person = $this->_get_person();
 
         $filePath = $this->_get_file_path();
         $path = $filePath['IMAGEDIRECTORY'];
         $file = $filePath['FILETEMPLATE'];
 
-        $row = $this->db->pq("SELECT ip FROM Pod WHERE filePath = :1 AND personId = :2 AND status = :3 ORDER BY created DESC LIMIT 1",
-                            array($path.$file, $person, 'Running'));
-        $this->_output($row);
+        $row = $this->db->pq("SELECT ip FROM Pod WHERE filePath LIKE CONCAT(:1, '%') AND personId = :2 AND status = :3 ORDER BY created DESC LIMIT 1",
+                            array($path, $person, 'Running'));
+
+        // Imperfect solution as it doesn't account for the visit number (app.vist on client side is broken)
+        if(strpos($path, $this->arg('prop')) === false) {
+            $this->_output();
+        } else {
+            $this->_output($row);
+        }
     }
 
     /**
