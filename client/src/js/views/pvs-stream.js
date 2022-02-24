dd90eb81bb28cf4aaa1822dbba509f2eafe40f61
Victor Nwaiwu <vonvikky@gmail.com>
2022-02-24 10:45:59 +0000

    fix: add more css classes to pvs stream view

--- client/src/js/views/pvs-stream.js
+++ client/src/js/views/pvs-stream.js
@@ -2,9 +2,9 @@ define(['marionette', 'collections/pvs'], function(Marionette, PVs) {
 
 
   const PVStreamItemView = Marionette.ItemView.extend({
-    template: _.template(`<h1><%- NAME %></h1> <p><%- VALUE %></p>`),
+    template: _.template(`<h1 class="tw-font-bold"><%- NAME %></h1> <p class="tw-text-xs"><%- VALUE %></p>`),
     className: function() {
-      return 'tw-w-full pv ' + this.model.get('CLASS')
+      return 'tw-w-full pv tw-my-4 tw-p-3 tw-rounded' + this.model.get('CLASS')
     },
 
     modelEvents: {
