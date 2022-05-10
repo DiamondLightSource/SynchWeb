9371c87990753d5c24029c4a8bc9b73023ae16fb
Nigel Wilson <nigel.wilson@diamond.ac.uk>
2022-05-10 16:05:25 +0100

    Added new templates

---
+++ client/src/js/modules/types/b18/menu.js
@@ -0,0 +1,24 @@
+/**
+ * The menu specification for b18 pages
+ */
+
+define([], function() {
+    
+    return {
+        menu: {
+            dc: 'View All Data',
+            visits: 'Visits',
+            contacts: 'Lab Contacts',
+            conexs: 'Conexs Submission',
+        },
+        
+        extra: {
+        
+        },
+        
+        admin: {
+            
+        },
+    }
+    
+})
