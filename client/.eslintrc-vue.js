246cf01c3be1b56e6acb0c9251431bd22ce6a460
Calum Byrom <calum.byrom@gmail.com>
2022-09-23 17:24:40 +0100

    Disable a couple of commonly abused vue rules

--- client/.eslintrc-vue.js
+++ client/.eslintrc-vue.js
@@ -7,6 +7,8 @@ module.exports = {
       rules: {
         // override/add rules settings here, such as:
         // 'vue/no-unused-vars': 'error'
+        'vue/require-default-prop': 0, // this is common throughout - should probably be fixed but pollutes output substantially so disabling for now
+        'vue/multi-word-component-names': 0 // ditto - though less important - this will require renaming lots of components to protect against quite a minor issue
       },
       env: {
         amd: true // registers globals for define and require
