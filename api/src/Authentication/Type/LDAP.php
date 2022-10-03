66fded12f28110ecbe261b1dc06b1d03513f9fa2
Stuart Campbell <scampbell@bnl.gov>
2022-10-03 15:46:24 -0400

    Add logic for Active Directory server being used as LDAP server

--- api/src/Authentication/Type/LDAP.php
+++ api/src/Authentication/Type/LDAP.php
@@ -15,7 +15,13 @@ class LDAP extends AuthenticationParent implements AuthenticationInterface
     function authenticate($login, $password)
     {
         global $ldap_server;
-        global $ldap_search;
+        global $ldap_server_type;
+	global $ldap_search;
+	global $active_directory_domain;
+
+	if (!$ldap_server_type) {
+		$ldap_search_type = "openldap";
+	}
 
         $conn = ldap_connect($ldap_server);
 
@@ -23,14 +29,27 @@ class LDAP extends AuthenticationParent implements AuthenticationInterface
             // Tested against LDAP version 3 (could add support for older versions here)
             ldap_set_option($conn, LDAP_OPT_PROTOCOL_VERSION, 3);
 
-            try {
-                // testing with openldap indicates this call needs to use a correct
-                // DN syntax: "uid=<login>,ou=people,dc=example,dc=com"
-                return ldap_bind($conn, "uid=" . $login . "," . $ldap_search, $password);
+	    try {
+		    if ($ldap_server_type == "activedirectory") {
+			    if (!$active_directory_domain) {
+				error_log("'active_directory_domain' parameter is not defined.");
+				error_log("\t This is required when LDAP server type is 'activedirectory'");
+				return false;
+			    }
+			$ldap_user = $active_directory_domain . "\\" . $login;
+		} else {
+			// testing with openldap indicates this call needs to use a correct
+			// DN syntax: "uid=<login>,ou=people,dc=example,dc=com"
+			$ldap_user = $login . "," . $ldap_search;
+		}
+                return ldap_bind($conn, $ldap_user, $password);
 
                 // Couldn't bind
-            } catch (\Exception $e) {
-                return false;
+	    } catch (\Exception $e) {
+		    error_log("SynchWeb - LDAP Auth FAILURE for user $login");
+		    error_log("\t" . $e->getMessage());
+		    error_log("\tldap_error: " . ldap_error($conn) . "  (Err Code: " . ldap_errno($conn) . ")");
+		    return false;
             }
         }
     }
