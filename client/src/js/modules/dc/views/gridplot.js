39d85c8876c9af28ad78628beab676efa115b80f
Mark Williams <mark.williams@diamond.ac.uk>
2023-03-02 15:48:51 +0000

    Use clearer variable names and just use dates to remove the 1e6 multiplier

--- client/src/js/modules/dc/views/gridplot.js
+++ client/src/js/modules/dc/views/gridplot.js
@@ -296,10 +296,10 @@ define(['jquery', 'marionette',
                 var w = bw*this.grid.get('STEPS_X')
                 var h = bh*this.grid.get('STEPS_Y')
 
-                var rts = this.grid.get('RTS')
-                var sged = app.options.get('scale_grid_end_date')
+                var start_date = this.grid.get('STARTDATE')
+                var scale_grid_end_date = app.options.get('scale_grid_end_date')
 
-                var scale_grid = (app.options.get('scale_grid').indexOf(this.getOption('BL')) > -1 && (rts < sged * 1e6 || sged == null))
+                var scale_grid = (app.options.get('scale_grid').indexOf(this.getOption('BL')) > -1 && (start_date < scale_grid_end_date || scale_grid_end_date == null))
                 console.log("scale_grid: "+scale_grid)
 
                 if (scale_grid) {
