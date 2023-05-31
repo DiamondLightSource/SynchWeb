c56b63473272912da8fa105ffc114fc6cea28990
Matthew Pritchard <46708056+MattPrit@users.noreply.github.com>
2023-05-31 14:39:04 +0100

    LIMS[682,688]Improvement: Seperate dispatch address fields, strip whitespace (#572)
    * LIMS-682: Make city and postcode sepearate dispatch fields
    
    * LIMS-682,688: Sepearte city and postcode, add stripping to postcode and address
    
    * Remove accidental comment
    
    * LIMS-682: Improve address formatting regex

--- api/src/Page/Shipment.php
+++ api/src/Page/Shipment.php
@@ -57,6 +57,8 @@ class Shipment extends Page
 
         'ADDRESS' => '.*',
         'COUNTRY' => '.*',
+        'CITY' => '([\w\s\-])+',
+        'POSTCODE' => '([\w\s\-])+',
         //   'DESCRIPTION' => '.*',
         'EMAILADDRESS' => '.*',
         'FAMILYNAME' => '.*',
@@ -940,6 +942,8 @@ class Shipment extends Page
         $shipment_data = array(
             "consignee_company_name" => $dispatch_info['LABNAME'],
             "consignee_country" => $dispatch_info['COUNTRY'],
+            "consignee_city" => $dispatch_info['CITY'],
+            "consignee_post_code" => Utils::getValueOrDefault($dispatch_info['POSTCODE'], null),
             "consignee_contact_name" =>  $dispatch_info['GIVENNAME'] . " " .  $dispatch_info['FAMILYNAME'],
             "consignee_contact_phone_number" =>  $dispatch_info['PHONENUMBER'],
             "consignee_contact_email" =>  $dispatch_info['EMAILADDRESS'],
@@ -959,15 +963,9 @@ class Shipment extends Page
         # Split up address. Necessary as address is a single field in ispyb
         $address_lines = explode(PHP_EOL, rtrim($dispatch_info['ADDRESS']));
         $num_lines = count($address_lines);
-        if ($num_lines < 3) {
-            throw new Exception("Could not build request for shipping service: address input does contain at least 3 lines (inc. city and post code)");
-        } else if ($num_lines > 5) {
-            throw new Exception("Could not build request for shipping service: address input contains more than 5 lines (inc. city and post code)");
+        if ($num_lines  > 3) {
+            throw new Exception("Could not build request for shipping service: address input contains more than 3 lines (exc. city and post code)");
         }
-        $shipment_data['consignee_post_code'] = $address_lines[$num_lines - 1];
-        unset($address_lines[$num_lines - 1]);
-        $shipment_data['consignee_city'] = $address_lines[$num_lines - 2];
-        unset($address_lines[$num_lines - 2]);
         if (isset($address_lines[0])) $shipment_data['consignee_address_line1'] = $address_lines[0];
         if (isset($address_lines[1])) $shipment_data['consignee_address_line2'] = $address_lines[1];
         if (isset($address_lines[2])) $shipment_data['consignee_address_line3'] = $address_lines[2];
@@ -1118,7 +1116,6 @@ class Shipment extends Page
             $data['LOCALCONTACT'] = $local_contact;
         if (!array_key_exists('LCEMAIL', $data))
             $data['LCEMAIL'] = '';
-        $data['ADDRESS'] = $data['ADDRESS'] . PHP_EOL . $country;
         $email->data = $data;
 
         if ($country != $facility_country && !is_null($dispatch_email_intl)) {
