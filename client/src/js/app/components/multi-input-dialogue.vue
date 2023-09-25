77198d06fb74ee84580a07aa7850e6ea213a4a6d
NKatti2011 <108797920+NKatti2011@users.noreply.github.com>
2023-09-25 16:28:38 +0100

    Update client/src/js/app/components/multi-input-dialogue.vue
    Co-authored-by: Matthew Pritchard <46708056+MattPrit@users.noreply.github.com>

--- client/src/js/app/components/multi-input-dialogue.vue
+++ client/src/js/app/components/multi-input-dialogue.vue
@@ -46,7 +46,6 @@ export default {
         showSelector: false,
         inputContent: '',
         selected: this.multInputSelected,
-        debounceTimeout: null,
         showDialog: false,
         }
     },
