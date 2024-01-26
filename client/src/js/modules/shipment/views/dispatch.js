40f737b5026fe1d605419ce1956313c293e56887
Matthew Pritchard <matthew.pritchard@diamond.ac.uk>
2024-01-26 15:06:19 +0000

    LIMS-1113: Add supported country dropdown to dispatch from

--- client/src/js/modules/shipment/views/dispatch.js
+++ client/src/js/modules/shipment/views/dispatch.js
@@ -60,7 +60,8 @@ define(['marionette', 'views/form',
             'click @ui.facc': 'showTerms',
             'blur @ui.postCode': 'stripPostCode',
             'blur @ui.addr': 'formatAddress',
-            'change @ui.country': 'checkPostCodeRequired'
+            'change @ui.country': 'checkPostCodeRequired',
+            'change @ui.dispatchCountry': 'showDispatchForm'
         },
         
         ui: {
@@ -90,10 +91,14 @@ define(['marionette', 'views/form',
             facilityCourier: '.facilityCourier',
             awbNumber: 'input[name=AWBNUMBER]',
             useAnotherCourierAccount: 'input[name=USE_ANOTHER_COURIER_ACCOUNT]',
-            dispatchState: '.dispatch-state'
+            dispatchState: '.dispatch-state',
+
+            dispatchCountry: 'select[name=DISPATCHCOUNTRY]',
+            courierSection: '.courierSection'
         },
 
         labContactCountry : null,
+        dispatchCountry: null,
 
         templateHelpers: function() {
             return {
@@ -118,7 +123,7 @@ define(['marionette', 'views/form',
             if (
                 app.options.get("shipping_service_app_url")
                 && (Number(this.terms.get('ACCEPTED')) === 1) // terms.ACCPETED could be undefined, 1, or "1"
-                && app.options.get("facility_courier_countries").includes(this.labContactCountry)
+                && app.options.get("facility_courier_countries").includes(this.dispatchCountry)
             ) {
                 this.getOption('dewar').fetch().done((dewar) => {
                     const external_id = dewar.EXTERNALSHIPPINGIDFROMSYNCHROTRON;
@@ -163,10 +168,20 @@ define(['marionette', 'views/form',
         doOnRender: function() {
             this.ui.exp.html(this.visits.opts()).val(this.model.get('VISIT'))
             this.updateLC()
+            this.populateDispatchCountries()
             this.populateCountries()
             this.stripPostCode()
             this.formatAddress()
             this.$el.show()
+            this.hideDispatchForm();
+        },
+
+        populateDispatchCountries: function () {
+            const dispatchCountryOptions = [...app.options.get("facility_courier_countries"), 'Other']
+                .map((country) => `<option>${country}</option>`)
+                .join("");
+            this.ui.dispatchCountry.html(dispatchCountryOptions);
+            this.ui.dispatchCountry.val('');
         },
 
         populateCountries: function() {
@@ -257,6 +272,28 @@ define(['marionette', 'views/form',
             }
         },
 
+        hideDispatchForm: function () {
+            this.ui.courierSection.hide();
+            this.ui.dispatchDetails.hide();
+            this.ui.submit.hide();
+        },
+
+        showDispatchForm: function() {
+            this.dispatchCountry = this.ui.dispatchCountry.val()
+            this.ui.courierSection.show();
+            this.ui.dispatchDetails.show();
+            this.ui.submit.show();
+            if (
+                this.terms.get("ACCEPTED")
+                && app.options.get("shipping_service_app_url")
+                && app.options.get("facility_courier_countries").includes(this.dispatchCountry)
+            ){
+                this.model.visitRequired = false
+                this.ui.dispatchDetails.hide()
+                this.ui.submit.text("Proceed")
+            }
+        },
+
         showTerms: function() {
             var terms = new TCDialog({ model: this.terms })
             this.listenTo(terms, 'terms:accepted', this.termsAccepted, this)
@@ -285,7 +322,7 @@ define(['marionette', 'views/form',
             this.model.courierDetailsRequired = false
             if (
                 app.options.get("shipping_service_app_url")
-                && app.options.get("facility_courier_countries").includes(this.labContactCountry)
+                && app.options.get("facility_courier_countries").includes(this.ui.dispatchCountry.val())
             ){
                 this.model.visitRequired = false
                 this.ui.dispatchDetails.hide()
