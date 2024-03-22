27616a462f49a940f12bb8f9ae877ffe2054beb0
Guilherme de Freitas <guilherme.de-freitas@diamond.ac.uk>
2024-03-22 16:43:39 +0000

    Remove EM related files

--- api/config_sample.php
+++ api/config_sample.php
@@ -309,7 +309,7 @@
 
     # These map proposal types to their proposalcode
     # - If these are not defined for a proposal type, the api then uses bl_types below
-    $prop_types = array('mx', 'em');
+    $prop_types = array('mx');
 
     # This maps beamlinename in blsession to a proposal type
     # - Internal maps a beamline to an api "type", there are currently:
---
+++ client/src/js/modules/types/em/dc/redirect.vue
@@ -0,0 +1,26 @@
+<template>
+    <span>
+  <h1 class="tw-text-center tw-text-lg tw-mb-2">EM views in SynchWeb are no longer supported, and have been superseded by PATo.</h1>
+  <a :href="redirectUrl"><h2 class="tw-text-center tw-text-md">Click here to be redirected to the current visit in PATo.</h2></a>
+</span>
+</template>
+
+<script>
+
+export default {
+    'name': 'EmRedirect',
+    'props': {
+        'collection': {
+            'type': Number,
+            'required': true,
+        },
+    },
+    'computed': {
+        redirectUrl: function () {
+            const visitStr = this.collection.queryParams.visit;
+            const [proposal, visit] = visitStr.split("-");
+            return `https://ebic-pato.diamond.ac.uk/proposals/${proposal}/sessions/${visit}`
+        }
+    },
+}
+</script>
