6bea65c77eeed1adde4a2413c8b0337af44d1733
Guilherme de Freitas <guilherme.de-freitas@diamond.ac.uk>
2024-05-01 09:51:00 +0100

    Restore EM menu in dropdown

---
+++ client/src/js/modules/types/em/menu.js
@@ -0,0 +1,21 @@
+define([], function() {
+    return {
+        menu: {
+            dc: 'View All Data',
+            visits: 'Visits',
+            cal: 'Calendar',
+            shipments: 'Shipments',
+            contacts: 'Lab Contacts',
+            'dewars/registry': 'Registered Dewars',
+            stats: 'Statistics',
+            migrate: 'Migrate',
+        },
+        extra: {
+            //projects: 'Projects',
+        },
+        admin: {
+            'dewars/overview': { title: 'Logistics', icon: 'fa-truck', permission: 'all_dewars' },
+            faults: { title: 'Fault Reports', icon: 'fa-tasks' },
+        },
+    }
+})
