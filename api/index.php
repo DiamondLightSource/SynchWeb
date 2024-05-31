168fa860f00d0c9b6955be9f9862943ae54839aa
Mark W <24956497+ndg63276@users.noreply.github.com>
2024-05-31 13:30:51 +0100

    LIMS-1203: Remove hidden temporary button to 'Use facility account' (#741)
    Co-authored-by: Mark Williams <mark.williams@diamond.ac.uk>

--- api/index.php
+++ api/index.php
@@ -69,7 +69,7 @@ function setupApplication($mode): Slim
     $app->get('/options', function () use ($app) {
         global $motd, $authentication_type, $cas_url, $cas_sso, $sso_url, $package_description,
             $facility_courier_countries, $facility_courier_countries_nde,
-            $dhl_enable, $dhl_link, $scale_grid, $scale_grid_end_date, $preset_proposal, $timezone,
+            $dhl_enable, $scale_grid, $scale_grid_end_date, $preset_proposal, $timezone,
             $valid_components, $enabled_container_types, $ifsummary, $synchweb_version, $redirects,
             $shipping_service_app_url, $use_shipping_service_redirect, $use_shipping_service_redirect_incoming_shipments;
         $app->contentType('application/json');
@@ -84,7 +84,6 @@ function setupApplication($mode): Slim
             'facility_courier_countries' => $facility_courier_countries,
             'facility_courier_countries_nde' => $facility_courier_countries_nde,
             'dhl_enable' => $dhl_enable,
-            'dhl_link' => $dhl_link,
             'scale_grid' => $scale_grid,
             'scale_grid_end_date' => $scale_grid_end_date,
             'preset_proposal' => $preset_proposal,
