47c533f52f30d6d012ae6c64cc0fcf11f8d51c40
Mark Williams <mark.williams@diamond.ac.uk>
2025-10-14 12:33:27 +0100

    LIMS-1431: Get MXMRRunId from autoProcProgramId

--- api/src/Downstream/Type/MetalId.php
+++ api/src/Downstream/Type/MetalId.php
@@ -9,8 +9,10 @@ class MetalId extends DownstreamPlugin {
     var $has_images = true;
     var $friendlyname = 'MetalId';
     var $has_mapmodel = array(1, 0);
+    var $_mrrun;
 
     function results() {
+        // TODO - Get values from MXMRRunBlob table rather than log file
         $log = $this->_get_attachments('metal_id.log');
         $headings = array('Peak', 'Height', 'RMSD', 'XYZ');
         $peaks = array($headings);
@@ -41,19 +43,61 @@ class MetalId extends DownstreamPlugin {
         return $results;
     }
 
+    function _find_mrrun() {
+        $mrrun = $this->db->pq(
+            "SELECT m.mxmrrunid
+            FROM mxmrrun m
+            WHERE m.autoprocprogramid=:1",
+            array($this->autoprocprogramid)
+        );
+
+        if (!sizeof($mrrun)) {
+            return;
+        }
+
+        $this->_mrrun = $mrrun[0];
+        return true;
+    }
+
+    function _get_blobs() {
+        return $this->db->pq(
+            "SELECT view1, filepath
+            FROM mxmrrunblob
+            WHERE mxmrrunid = :1
+            AND filepath is not NULL",
+            array($this->_mrrun["MXMRRUNID"])
+        );
+    }
+
+
     function images($n = 0) {
-        $png = $this->_get_attachments(null, null, '.png');
-        if ($png) {
-            return $png[$n]['FILE'];
-        } else {
-            return '';
+        if (!$this->_find_mrrun()) {
+            return;
+        }
+
+        $blobs = $this->_get_blobs();
+        if (sizeof($blobs)) {
+            $views = array("VIEW1");
+            if ($n < sizeof($views)) {
+                return $blobs[0]["FILEPATH"] .
+                    '/' .
+                    $blobs[0][$views[$n]];
+            }
         }
     }
+
 
     function mapmodel($n = 0, $map = false) {
         $pdb = $this->_get_attachments('final.pdb');
         if (!$pdb) {
             return;
+        }
+        if (true) {
+            $mtz = $this->_get_attachments('diff.map');
+            if (!$mtz) {
+                return;
+            }
+            return $mtz['FILE'];
         } else {
             return $pdb['FILE'];
         }
