9bc86d58d58b66e5499811c6b41685df8269946e
Mark W <24956497+ndg63276@users.noreply.github.com>
2025-12-09 11:00:47 +0000

    LIMS-1818: Add ability to download CSV of dispensing positions (#1001)
    * LIMS-1818: Allow download of CSV of dispensing positions
    
    * LIMS-1818: Always show CSV button
    
    * LIMS-1818: Rename file, update drop names, add blank rows
    
    * LIMS-1818: Print commas on empty lines
    
    * Fix mime type
    
    Co-authored-by: Guilherme Francisco <guilherme.de-freitas@diamond.ac.uk>
    
    * LIMS-1818: Remove empty lines entirely
    
    ---------
    
    Co-authored-by: Mark Williams <mark.williams@diamond.ac.uk>
    Co-authored-by: Guilherme Francisco <guilherme.de-freitas@diamond.ac.uk>

--- api/src/Page/Download.php
+++ api/src/Page/Download.php
@@ -35,6 +35,7 @@ class Download extends Page
 
         'ppl' => '\w+',
         'aid' => '\w+',
+        'cid' => '\d+',
 
         'filetype' => '\w+',
         'blsampleid' => '\d+',
@@ -49,6 +50,7 @@ class Download extends Page
     public static $dispatch = array(
         array('/plots', 'get', '_auto_processing_plots'),
         array('/csv/visit/:visit', 'get', '_csv_report'),
+        array('/csv/container/:cid', 'get', '_dispensing_csv'),
         array('/sign', 'post', '_sign_url'),
         array('/data/visit/:visit', 'get', '_download_visit'),
         array('/attachments', 'get', '_get_attachments'),
@@ -321,6 +323,52 @@ class Download extends Page
         foreach ($rows as $r) {
             $r['COMMENTS'] = '"' . $r['COMMENTS'] . '"';
             print implode(',', array_values($r)) . "\n";
+        }
+    }
+
+    # ------------------------------------------------------------------------
+    # CSV Report of dispensing positions for a plate
+    function _dispensing_csv()
+    {
+        if (!$this->has_arg('cid'))
+            $this->_error('No container id specified');
+        $rows = $this->db->pq("SELECT c.code, s.location, ct.name, ct.capacity, ct.wellperrow,
+                bsp.posx, bsp.posy, si.imagefullpath, si.micronsperpixelx, si.micronsperpixely
+                FROM blsample s
+                INNER JOIN container c ON c.containerid = s.containerid
+                INNER JOIN blsampleimage si ON si.blsampleid = s.blsampleid
+                LEFT OUTER JOIN containertype ct ON (c.containertypeid IS NOT NULL AND c.containertypeid = ct.containertypeid) OR (c.containertypeid IS NULL AND c.containertype = ct.name)
+                LEFT OUTER JOIN
+                    (SELECT * FROM blsampleposition bsp1 WHERE bsp1.blsamplepositionid =
+                        (SELECT MAX(blsamplepositionid) FROM blsampleposition bsp2 WHERE bsp2.blsampleid = bsp1.blsampleid AND bsp2.positiontype='dispensing')
+                    ) bsp ON bsp.blsampleid = s.blsampleid
+                WHERE c.containerid=:1
+                ORDER BY s.location+0",
+                array($this->arg('cid')));
+        $plate = $rows[0];
+        $rowNames = range("A", "H");
+        $dropNames = range("a", "z");
+        // SWISSCI 3 Drop have drops a/c/d
+        if ($plate["NAME"] == "SWISSCI 3 Drop") {
+            $dropNames = array_merge(array("a"), range("c","z"));
+        }
+        $dropsPerWell = $plate["CAPACITY"] / (count($rowNames) * $plate["WELLPERROW"]);
+        $this->app->response->headers->set("Content-type", "text/csv");
+        $this->_set_disposition_attachment($this->app->response, $plate["CODE"] . "_targets.csv");
+        list($width, $height, $type, $attr) = getimagesize($plate['IMAGEFULLPATH']);
+        foreach ($rows as $r) {
+            if (!isset($r["POSX"]) || !isset($r["POSY"])) {
+                continue; # skip empty rows
+            }
+            $wellNumber = intval(($r["LOCATION"] - 1) / $dropsPerWell);  # 0 indexed
+            $rowNumber = intval($wellNumber / $plate["WELLPERROW"]);  # 0 indexed
+            $row = $rowNames[$rowNumber];
+            $column = str_pad($wellNumber - ($rowNumber * $plate["WELLPERROW"]) + 1, 2, 0, STR_PAD_LEFT);  # pad with a zero if needed
+            $dropNumber = intval($r["LOCATION"] - ($dropsPerWell * $wellNumber));  # 1 indexed
+            $drop = $dropNames[$dropNumber-1];
+            $xval = round(($r["POSX"] - $width/2) * $r["MICRONSPERPIXELX"]); # integers
+            $yval = round(($height/2 - $r["POSY"]) * $r["MICRONSPERPIXELY"]); # integers
+            print $row . $column . $drop . "," . $xval . "," . $yval . "\n";
         }
     }
 
